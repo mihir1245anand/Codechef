f21c48715e26f35dab170560dd76f4273cde4eaa
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:53 +0530

    Add solution for Cricket World Cup Qualifier (CWC23QUALIF)

---
+++ Cricket World Cup Qualifier.cpp
@@ -0,0 +1,21 @@
+/**
+ * Problem Name: Cricket World Cup Qualifier
+ * Problem Code: CWC23QUALIF
+ * Problem URL: https://www.codechef.com/problems/CWC23QUALIF
+ * Submission ID: 1324592680
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	
+	if(x>=12)
+	cout<<"Yes";
+	else
+	cout<<"No";
+
+}
