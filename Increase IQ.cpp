50d18ab5962aa41e9adbde1ca0f84873281f97ce
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:13 +0530

    Add solution for Increase IQ (INCRIQ)

---
+++ Increase IQ.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: Increase IQ
+ * Problem Code: INCRIQ
+ * Problem URL: https://www.codechef.com/problems/INCRIQ
+ * Submission ID: 1331110499
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X + 7 > 170)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
