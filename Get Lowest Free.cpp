73c9a4d5759cd387efd3f07c037f900ca0fb5d5f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:09 +0530

    Add solution for Get Lowest Free (SALE)

---
+++ Get Lowest Free.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Get Lowest Free
+ * Problem Code: SALE
+ * Problem URL: https://www.codechef.com/problems/SALE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Get Lowest Free (SALE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
