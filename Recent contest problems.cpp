9d7e7696f01658994ae033aebd5dd26ccdea59e4
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:45 +0530

    Add solution for Recent contest problems (RECENTCONT)

---
+++ Recent contest problems.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Recent contest problems
+ * Problem Code: RECENTCONT
+ * Problem URL: https://www.codechef.com/problems/RECENTCONT
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Recent contest problems (RECENTCONT)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
