62ab27a5a218d8b373bbae12a6bdfa20e2d5e2e1
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:20 +0530

    Add solution for Air Quality Index (AIRINDEX)

---
+++ Air Quality Index.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: Air Quality Index
+ * Problem Code: AIRINDEX
+ * Problem URL: https://www.codechef.com/problems/AIRINDEX
+ * Submission ID: 1330643259
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X < 100)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
