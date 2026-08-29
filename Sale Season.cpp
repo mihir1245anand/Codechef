9094794856b3a219b88093f40013e728bf600ad6
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:48 +0530

    Add solution for Sale Season (SALESEASON)

---
+++ Sale Season.cpp
@@ -0,0 +1,31 @@
+/**
+ * Problem Name: Sale Season
+ * Problem Code: SALESEASON
+ * Problem URL: https://www.codechef.com/problems/SALESEASON
+ * Submission ID: 1325598082
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X <= 100)
+            cout << X << endl;
+        else if (X <= 1000)
+            cout << X - 25 << endl;
+        else if (X <= 5000)
+            cout << X - 100 << endl;
+        else
+            cout << X - 500 << endl;
+    }
+
+    return 0;
+}
