9c54654371d3a3fb0c4c7c1b363c854e9f8683ce
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:09 +0530

    Add solution for Water Filling (WATERFILLING)

---
+++ Water Filling.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Water Filling
+ * Problem Code: WATERFILLING
+ * Problem URL: https://www.codechef.com/problems/WATERFILLING
+ * Submission ID: 1325597191
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int B1, B2, B3;
+        cin >> B1 >> B2 >> B3;
+
+        int empty = (B1 == 0) + (B2 == 0) + (B3 == 0);
+
+        if (empty >= 2)
+            cout << "Water filling time" << endl;
+        else
+            cout << "Not now" << endl;
+    }
+
+    return 0;
+}
