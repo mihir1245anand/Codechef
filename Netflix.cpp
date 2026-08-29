484ded061e94475fa85b9ece0eab96cebc89563e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:31 +0530

    Add solution for Netflix (NETFLIX)

---
+++ Netflix.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Netflix
+ * Problem Code: NETFLIX
+ * Problem URL: https://www.codechef.com/problems/NETFLIX
+ * Submission ID: 1331116548
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C, X;
+        cin >> A >> B >> C >> X;
+
+        if (A + B >= X || A + C >= X || B + C >= X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
