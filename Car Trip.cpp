5b5483f5c65649346386982962a1ba7068452d3b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:35 +0530

    Add solution for Car Trip (CARTRIP)

---
+++ Car Trip.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Car Trip
+ * Problem Code: CARTRIP
+ * Problem URL: https://www.codechef.com/problems/CARTRIP
+ * Submission ID: 1330661186
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << max(X, 300) * 10 << endl;
+    }
+
+    return 0;
+}
