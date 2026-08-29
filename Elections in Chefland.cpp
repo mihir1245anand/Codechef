bb737947060488d171d0e873b822adc53f595bde
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:59 +0530

    Add solution for Elections in Chefland (ELECTN)

---
+++ Elections in Chefland.cpp
@@ -0,0 +1,34 @@
+/**
+ * Problem Name: Elections in Chefland
+ * Problem Code: ELECTN
+ * Problem URL: https://www.codechef.com/problems/ELECTN
+ * Submission ID: 1343832499
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            int age;
+            cin >> age;
+
+            if (age >= X)
+                count++;
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
