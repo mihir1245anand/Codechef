1ed56ac83288a727298d96be1d6ab5a886ee8a66
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:48 +0530

    Add solution for Circular Track (LOOP)

---
+++ Circular Track.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Circular Track
+ * Problem Code: LOOP
+ * Problem URL: https://www.codechef.com/problems/LOOP
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Circular Track (LOOP)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
