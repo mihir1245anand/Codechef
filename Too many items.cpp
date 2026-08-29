211b30ebe631df0e0d2f7d440b80fa62a633f582
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:03 +0530

    Add solution for Too many items (POLYBAGS)

---
+++ Too many items.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Too many items
+ * Problem Code: POLYBAGS
+ * Problem URL: https://www.codechef.com/problems/POLYBAGS
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Too many items (POLYBAGS)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
