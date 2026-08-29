45f8923624bf227056d5eb3ea61aed459acc8d48
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:11 +0530

    Add solution for Grade The Steel (FLOW014)

---
+++ Grade The Steel.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Grade The Steel
+ * Problem Code: FLOW014
+ * Problem URL: https://www.codechef.com/problems/FLOW014
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Grade The Steel (FLOW014)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
