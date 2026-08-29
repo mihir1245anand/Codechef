8fe11c07a4db070d6844742e7619755829333977
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:29 +0530

    Add solution for Bob at the Bank (BOBBANK)

---
+++ Bob at the Bank.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Bob at the Bank
+ * Problem Code: BOBBANK
+ * Problem URL: https://www.codechef.com/problems/BOBBANK
+ * Submission ID: 1331111875
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long W, X, Y, Z;
+        cin >> W >> X >> Y >> Z;
+
+        cout << W + (X - Y) * Z << endl;
+    }
+
+    return 0;
+}
