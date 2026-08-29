2a62381eece23a665e7980c7920377ca60735997
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:43 +0530

    Add solution for Rain in Chefland (RAINFALL1)

---
+++ Rain in Chefland.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Rain in Chefland
+ * Problem Code: RAINFALL1
+ * Problem URL: https://www.codechef.com/problems/RAINFALL1
+ * Submission ID: 1330637316
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X < 3)
+            cout << "LIGHT" << endl;
+        else if (X < 7)
+            cout << "MODERATE" << endl;
+        else
+            cout << "HEAVY" << endl;
+    }
+
+    return 0;
+}
