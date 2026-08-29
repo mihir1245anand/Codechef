6ce4ee30d9f75f7d8e986853abfaca5da39a7ef9
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:09 +0530

    Add solution for Watching Movies at 2x (MOVIE2X)

---
+++ Watching Movies at 2x.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Name: Watching Movies at 2x
+ * Problem Code: MOVIE2X
+ * Problem URL: https://www.codechef.com/problems/MOVIE2X
+ * Submission ID: 1346033919
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << X - Y / 2 << endl;
+
+    return 0;
+}
