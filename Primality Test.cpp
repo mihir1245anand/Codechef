e56691d969d17a8986dd889e1aafb521d7148589
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:39 +0530

    Add solution for Primality Test (PRB01)

---
+++ Primality Test.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Primality Test
+ * Problem Code: PRB01
+ * Problem URL: https://www.codechef.com/problems/PRB01
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Primality Test (PRB01)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
