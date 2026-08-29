9ff79dd025bcfb7618830af8cebd5af97905a584
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:25 +0530

    Add solution for Battery Health (BTRYHLTH)

---
+++ Battery Health.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Battery Health
+ * Problem Code: BTRYHLTH
+ * Problem URL: https://www.codechef.com/problems/BTRYHLTH
+ * Submission ID: 1330623864
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X >= 80)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
