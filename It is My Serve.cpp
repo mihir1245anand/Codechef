e890667a8c5ceb8525387d016879e4a8ad8f1283
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:17 +0530

    Add solution for It is My Serve (MYSERVE)

---
+++ It is My Serve.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: It is My Serve
+ * Problem Code: MYSERVE
+ * Problem URL: https://www.codechef.com/problems/MYSERVE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: It is My Serve (MYSERVE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
