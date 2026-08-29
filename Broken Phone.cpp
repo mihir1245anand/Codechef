0314b8ddfc896949c4778f7701498cae2bb58b4d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:30 +0530

    Add solution for Broken Phone (BROKENPHONE)

---
+++ Broken Phone.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Broken Phone
+ * Problem Code: BROKENPHONE
+ * Problem URL: https://www.codechef.com/problems/BROKENPHONE
+ * Submission ID: 1330680144
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X < Y)
+            cout << "REPAIR" << endl;
+        else if (Y < X)
+            cout << "NEW PHONE" << endl;
+        else
+            cout << "ANY" << endl;
+    }
+
+    return 0;
+}
