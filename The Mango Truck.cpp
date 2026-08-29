c14b4ab5320e9229f02ea4bad43d0ab0ed157911
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:01 +0530

    Add solution for The Mango Truck (MANGOES)

---
+++ The Mango Truck.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: The Mango Truck
+ * Problem Code: MANGOES
+ * Problem URL: https://www.codechef.com/problems/MANGOES
+ * Submission ID: 1331112534
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        cout << (Z - Y) / X << endl;
+    }
+
+    return 0;
+}
