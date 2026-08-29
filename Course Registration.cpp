cd8fb52f30a58c631590f1100506bd900a3c271a
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:52 +0530

    Add solution for Course Registration (COURSEREG)

---
+++ Course Registration.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Course Registration
+ * Problem Code: COURSEREG
+ * Problem URL: https://www.codechef.com/problems/COURSEREG
+ * Submission ID: 1331106699
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M, K;
+        cin >> N >> M >> K;
+
+        if (N <= M - K)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+}
