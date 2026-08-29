f6913b6c8f3f4289c6bac5d2877ba366822eef92
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:41 +0530

    Add solution for Chef And Operators (CHOPRT)

---
+++ Chef And Operators.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef And Operators
+ * Problem Code: CHOPRT
+ * Problem URL: https://www.codechef.com/problems/CHOPRT
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef And Operators (CHOPRT)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
