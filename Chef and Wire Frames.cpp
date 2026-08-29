f30aedffbd0a4fe079cf81e315cbb8a100d27bee
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:43 +0530

    Add solution for Chef and Wire Frames (CWIREFRAME)

---
+++ Chef and Wire Frames.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Chef and Wire Frames
+ * Problem Code: CWIREFRAME
+ * Problem URL: https://www.codechef.com/problems/CWIREFRAME
+ * Submission ID: 1330662569
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M, X;
+        cin >> N >> M >> X;
+
+        cout << 2 * (N + M) * X << endl;
+    }
+
+    return 0;
+}
