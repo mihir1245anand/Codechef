9be1583790db31a5be1ea31dcaa68f370098d28d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:36 +0530

    Add solution for Peaceful Party (MAYOR_PARTY)

---
+++ Peaceful Party.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Peaceful Party
+ * Problem Code: MAYOR_PARTY
+ * Problem URL: https://www.codechef.com/problems/MAYOR_PARTY
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Peaceful Party (MAYOR_PARTY)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
