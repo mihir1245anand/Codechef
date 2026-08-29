489688f828cacb832764f31cbff3f6d789347115
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:29 +0530

    Add solution for Monopoly (MONOPOLY2)

---
+++ Monopoly.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Name: Monopoly
+ * Problem Code: MONOPOLY2
+ * Problem URL: https://www.codechef.com/problems/MONOPOLY2
+ * Submission ID: 1340604973
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int P, Q, R, S;
+        cin >> P >> Q >> R >> S;
+
+        int total = P + Q + R + S;
+        int mx = max({P, Q, R, S});
+
+        if (mx > total - mx)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
