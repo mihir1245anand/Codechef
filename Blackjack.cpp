f15c2e7339f6c6ace3ab8659b2f0b01c0c39f86f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:29 +0530

    Add solution for Blackjack (BLACKJACK)

---
+++ Blackjack.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Blackjack
+ * Problem Code: BLACKJACK
+ * Problem URL: https://www.codechef.com/problems/BLACKJACK
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Blackjack (BLACKJACK)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
