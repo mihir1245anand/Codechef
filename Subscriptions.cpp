0ea19cb03f5b6833507df7daec0d2d10ae0bae3b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:55 +0530

    Add solution for Subscriptions (SUBSCRIBE_)

---
+++ Subscriptions.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Subscriptions
+ * Problem Code: SUBSCRIBE_
+ * Problem URL: https://www.codechef.com/problems/SUBSCRIBE_
+ * Submission ID: 1332559646
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        int subscriptions = (N + 5) / 6;   
+        cout << subscriptions * X << endl;
+    }
+
+    return 0;
+}
