dc6076a3d8e65a63acc9b8bfdd4902aa26233e48
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:33 +0530

    Add solution for Buy 2 Get 1 Free (SALE2)

---
+++ Buy 2 Get 1 Free.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Buy 2 Get 1 Free
+ * Problem Code: SALE2
+ * Problem URL: https://www.codechef.com/problems/SALE2
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Buy 2 Get 1 Free (SALE2)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
