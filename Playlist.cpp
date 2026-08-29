04cfbb86b2547a383068b9a8df24adf660bb69ea
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:37 +0530

    Add solution for Playlist (SONGS)

---
+++ Playlist.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Playlist
+ * Problem Code: SONGS
+ * Problem URL: https://www.codechef.com/problems/SONGS
+ * Submission ID: 1331115267
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        cout << N / (3 * X) << endl;
+    }
+
+    return 0;
+}
