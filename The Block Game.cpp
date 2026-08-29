41ee89c283c59647410c31dd8634b63106c36830
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:58 +0530

    Add solution for The Block Game (PALL01)

---
+++ The Block Game.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: The Block Game
+ * Problem Code: PALL01
+ * Problem URL: https://www.codechef.com/problems/PALL01
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: The Block Game (PALL01)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
