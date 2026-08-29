c3e1fc9149ce1c9c66bd7bf273799bf8de9ef26a
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:21 +0530

    Add solution for Mario and Bullet (BULLET)

---
+++ Mario and Bullet.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Mario and Bullet
+ * Problem Code: BULLET
+ * Problem URL: https://www.codechef.com/problems/BULLET
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Mario and Bullet (BULLET)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
