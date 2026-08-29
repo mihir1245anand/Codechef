274dd786572144d76d77eea62a58a92d8b0718bc
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:49 +0530

    Add solution for Second Max of Three Numbers (SNDMAX)

---
+++ Second Max of Three Numbers.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Second Max of Three Numbers
+ * Problem Code: SNDMAX
+ * Problem URL: https://www.codechef.com/problems/SNDMAX
+ * Submission ID: 1330628146
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    while (N--) {
+        int a[3];
+        cin >> a[0] >> a[1] >> a[2];
+
+        sort(a, a + 3);
+        cout << a[1] << endl;
+    }
+
+    return 0;
+}
