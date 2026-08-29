c510252fbf5150d6f898b5a90ba47b78f447545d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:38 +0530

    Add solution for Chef and Contest (CHEFCONTEST)

---
+++ Chef and Contest.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and Contest
+ * Problem Code: CHEFCONTEST
+ * Problem URL: https://www.codechef.com/problems/CHEFCONTEST
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef and Contest (CHEFCONTEST)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
