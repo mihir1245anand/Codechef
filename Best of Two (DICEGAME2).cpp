c7cb4ae4fbd5642ee25b070a6d7e2a389e211f17
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:27 +0530

    Add solution for Best of Two (DICEGAME2)

---
+++ Best of Two (DICEGAME2).cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Best of Two
+ * Problem Code: DICEGAME2
+ * Problem URL: https://www.codechef.com/problems/DICEGAME2
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Best of Two (DICEGAME2)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
