f0b2ce0d4c5bc9cd9ecf40cc0decd47a80f78391
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:52 +0530

    Add solution for Six Friends (SIXFRIENDS)

---
+++ Six Friends.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Six Friends
+ * Problem Code: SIXFRIENDS
+ * Problem URL: https://www.codechef.com/problems/SIXFRIENDS
+ * Submission ID: 1330662291
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << min(3 * X, 2 * Y) << endl;
+    }
+
+    return 0;
+}
