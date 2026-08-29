f33d002cf7269660ee386a25865812b137d4ce02
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:32 +0530

    Add solution for Bus Rows (BUSROW)

---
+++ Bus Rows.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Bus Rows
+ * Problem Code: BUSROW
+ * Problem URL: https://www.codechef.com/problems/BUSROW
+ * Submission ID: 1341024188
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+	void solve(){
+	    long long N,M,X;
+	    cin>>N>>M>>X;
+	    
+	    long long R=(X+M-1)/M;
+	    long long ans=min(R,N-R+1);
+	    cout<<ans<<"\n";
+	}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        solve();
+    }
+}
