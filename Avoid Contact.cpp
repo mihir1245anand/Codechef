182de62c976a1a1313e972e6240bfdd45fda9718
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:23 +0530

    Add solution for Avoid Contact (AVOIDCONTACT)

---
+++ Avoid Contact.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Avoid Contact
+ * Problem Code: AVOIDCONTACT
+ * Problem URL: https://www.codechef.com/problems/AVOIDCONTACT
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Avoid Contact (AVOIDCONTACT)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
