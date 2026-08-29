136cf4dba4f7990e4443125c91e63bbb1d432937
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:27 +0530

    Add solution for Best of Two (BESTOFTWO)

---
+++ Best of Two.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Best of Two
+ * Problem Code: BESTOFTWO
+ * Problem URL: https://www.codechef.com/problems/BESTOFTWO
+ * Submission ID: 1330587822
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << max(X, Y) << endl;
+    }
+
+    return 0;
+}
