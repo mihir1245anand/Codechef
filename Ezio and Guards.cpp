93759b195fa44fc523154aca70f4e38fa655086e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:03 +0530

    Add solution for Ezio and Guards (MANIPULATE)

---
+++ Ezio and Guards.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Ezio and Guards
+ * Problem Code: MANIPULATE
+ * Problem URL: https://www.codechef.com/problems/MANIPULATE
+ * Submission ID: 1330676560
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X >= Y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
