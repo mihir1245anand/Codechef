5b68efbef9f188f88b74e9467e44a68ad1b1847f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:12 +0530

    Add solution for X Jumps (XJUMP)

---
+++ X Jumps.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: X Jumps
+ * Problem Code: XJUMP
+ * Problem URL: https://www.codechef.com/problems/XJUMP
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: X Jumps (XJUMP)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
