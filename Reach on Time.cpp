16dd97cdb214576c5d7174d4d643b2d1dd69f7b2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:44 +0530

    Add solution for Reach on Time (TIMELY)

---
+++ Reach on Time.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Reach on Time
+ * Problem Code: TIMELY
+ * Problem URL: https://www.codechef.com/problems/TIMELY
+ * Submission ID: 1329309978
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X >= 30)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
