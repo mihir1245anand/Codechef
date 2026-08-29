fd7e7254513ad5f6db29315df50394c450951e82
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:47 +0530

    Add solution for Chess Time (CHESSTIME)

---
+++ Chess Time.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Chess Time
+ * Problem Code: CHESSTIME
+ * Problem URL: https://www.codechef.com/problems/CHESSTIME
+ * Submission ID: 1330640979
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << 3 * N << endl;
+    }
+
+    return 0;
+}
