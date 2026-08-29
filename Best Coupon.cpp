b84b7e37633f7877da4b83633c0f3eb6604edbb3
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:26 +0530

    Add solution for Best Coupon (CHEAPFOOD)

---
+++ Best Coupon.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Best Coupon
+ * Problem Code: CHEAPFOOD
+ * Problem URL: https://www.codechef.com/problems/CHEAPFOOD
+ * Submission ID: 1331118050
+ * Language: C++
+ */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << max(X / 10, 100) << endl;
+    }
+
+    return 0;
+}
