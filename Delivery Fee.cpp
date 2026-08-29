50a4e1e27329c13302ba047fd1a608c801d9402c
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:55 +0530

    Add solution for Delivery Fee (DLFEE)

---
+++ Delivery Fee.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Delivery Fee
+ * Problem Code: DLFEE
+ * Problem URL: https://www.codechef.com/DSAMONDAY015/problems/DLFEE
+ * Contest: DSAMONDAY015
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            // Problem: Delivery Fee (DLFEE)
+        }
+    }
+    return 0;
+}
