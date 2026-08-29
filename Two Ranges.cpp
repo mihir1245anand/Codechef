c419676449cb843c783e301b6303d10a3d14c9fe
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:06 +0530

    Add solution for Two Ranges (TWORANGES)

---
+++ Two Ranges.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Two Ranges
+ * Problem Code: TWORANGES
+ * Problem URL: https://www.codechef.com/problems/TWORANGES
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Two Ranges (TWORANGES)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
