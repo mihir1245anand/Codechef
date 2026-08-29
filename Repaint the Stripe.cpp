8e42210f30c37547c800802c514c06bec14a0549
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:46 +0530

    Add solution for Repaint the Stripe (RPTS)

---
+++ Repaint the Stripe.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Repaint the Stripe
+ * Problem Code: RPTS
+ * Problem URL: https://www.codechef.com/DSAMONDAY015/problems/RPTS
+ * Contest: DSAMONDAY015
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            // Problem: Repaint the Stripe (RPTS)
+        }
+    }
+    return 0;
+}
