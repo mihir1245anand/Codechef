b07cec1605c0a52c7b3c9ddb0d90f3051806ac1d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:09 +0530

    Add solution for Genes (GENE01)

---
+++ Genes.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Genes
+ * Problem Code: GENE01
+ * Problem URL: https://www.codechef.com/problems/GENE01
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Genes (GENE01)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
