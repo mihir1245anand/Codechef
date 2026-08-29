37491446bca01b3be9bbb8d179713476ff1ffd52
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:32 +0530

    Add solution for October Marathon (OCTATHON)

---
+++ October Marathon.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: October Marathon
+ * Problem Code: OCTATHON
+ * Problem URL: https://www.codechef.com/problems/OCTATHON
+ * Submission ID: 1330634789
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int X;
+    cin >> X;
+
+    if (X < 3)
+        cout << "GOLD";
+    else if (X < 6)
+        cout << "SILVER";
+    else
+        cout << "BRONZE";
+
+    return 0;
+}
