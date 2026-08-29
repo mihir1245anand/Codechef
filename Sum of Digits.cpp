fbee98c8c353e4b23ddcfb9960c66f8bce4cc5ef
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:56 +0530

    Add solution for Sum of Digits (FLOW006)

---
+++ Sum of Digits.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Name: Sum of Digits
+ * Problem Code: FLOW006
+ * Problem URL: https://www.codechef.com/problems/FLOW006
+ * Submission ID: 1331101810
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        int sum = 0;
+        while (N > 0) {
+            sum += N % 10;
+            N /= 10;
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
