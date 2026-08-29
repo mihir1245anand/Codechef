b091ce704bdbf7eb0c2d3e879b80f4d9ea5a47ef
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:38 +0530

    Add solution for Chef and Chocolates (CHEFCHOCO)

---
+++ Chef and Chocolates.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Chef and Chocolates
+ * Problem Code: CHEFCHOCO
+ * Problem URL: https://www.codechef.com/problems/CHEFCHOCO
+ * Submission ID: 1331116097
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+    cin >> T;
+
+    while (T--) {
+        int C, X, Y;
+        cin >> C >> X >> Y;
+
+        cout << (C - X) * Y << endl;
+    }
+
+}
