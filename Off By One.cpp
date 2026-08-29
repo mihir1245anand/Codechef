7e4b1247725a89b4a4e73f09c8b8c0b7202023b0
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:32 +0530

    Add solution for Off By One (OFFBY1)

---
+++ Off By One.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: Off By One
+ * Problem Code: OFFBY1
+ * Problem URL: https://www.codechef.com/problems/OFFBY1
+ * Submission ID: 1325588457
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << (A + B) * 10 + 1 << endl;
+    return 0;
+}
