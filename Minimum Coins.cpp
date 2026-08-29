8fb5828b3b26a0b2b7382abc417539a833bf672d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:26 +0530

    Add solution for Minimum Coins (MINCOINSREQ)

---
+++ Minimum Coins.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Minimum Coins
+ * Problem Code: MINCOINSREQ
+ * Problem URL: https://www.codechef.com/problems/MINCOINSREQ
+ * Submission ID: 1330663519
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << X % 10 << endl;
+    }
+
+    return 0;
+}
