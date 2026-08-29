2ff1e872633b219c8ecb095dce4f94079a6a1a0e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:06 +0530

    Add solution for Find Remainder (FLOW002)

---
+++ Find Remainder.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Find Remainder
+ * Problem Code: FLOW002
+ * Problem URL: https://www.codechef.com/problems/FLOW002
+ * Submission ID: 1330675854
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+
+        cout << A % B << endl;
+    }
+
+    return 0;
+}
