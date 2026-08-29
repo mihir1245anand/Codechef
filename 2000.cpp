431c242a2cc72aa6becb4e2a29a8f5ae9815f47d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:18 +0530

    Add solution for 2000 (RIP2000)

---
+++ 2000.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: 2000
+ * Problem Code: RIP2000
+ * Problem URL: https://www.codechef.com/problems/RIP2000
+ * Submission ID: 1330594056
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cout << N * 4 << endl;
+    return 0;
+}
