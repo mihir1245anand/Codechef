75237abe7825aba4143133e9760b38da71d2f41e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:33 +0530

    Add solution for OFFICE (OFFICE)

---
+++ OFFICE.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: OFFICE
+ * Problem Code: OFFICE
+ * Problem URL: https://www.codechef.com/problems/OFFICE
+ * Submission ID: 1332566694
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << 4 * X + Y << endl;
+    }
+
+    return 0;
+}
