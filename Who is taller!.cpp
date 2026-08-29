900141a406e46ac94362f02efa196c7d7c3a9f46
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:11 +0530

    Add solution for Who is taller! (TALLER)

---
+++ Who is taller!.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Who is taller!
+ * Problem Code: TALLER
+ * Problem URL: https://www.codechef.com/problems/TALLER
+ * Submission ID: 1330581352
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X > Y)
+            cout << "A" << endl;
+        else
+            cout << "B" << endl;
+    }
+
+    return 0;
+}
