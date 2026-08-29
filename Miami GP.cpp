d4d5afb3d556a824fb74fb06b3358c59f592cbc0
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:25 +0530

    Add solution for Miami GP (F1RULE)

---
+++ Miami GP.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Miami GP
+ * Problem Code: F1RULE
+ * Problem URL: https://www.codechef.com/problems/F1RULE
+ * Submission ID: 1331114273
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (100 * Y <= 107 * X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
