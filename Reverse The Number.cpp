302e1e530093f69f6426e6699ead6cc2b6af415c
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:46 +0530

    Add solution for Reverse The Number (FLOW007)

---
+++ Reverse The Number.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Name: Reverse The Number
+ * Problem Code: FLOW007
+ * Problem URL: https://www.codechef.com/problems/FLOW007
+ * Submission ID: 1343212998
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        int rev = 0;
+        while (N > 0) {
+            rev = rev * 10 + (N % 10);
+            N /= 10;
+        }
+
+        cout << rev << endl;
+    }
+
+    return 0;
+}
