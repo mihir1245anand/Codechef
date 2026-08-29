fd43a3fbc21f78bca868e4245ec03f874e6d187f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:35 +0530

    Add solution for Car or Bike (TRAVELFAST)

---
+++ Car or Bike.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Car or Bike
+ * Problem Code: TRAVELFAST
+ * Problem URL: https://www.codechef.com/problems/TRAVELFAST
+ * Submission ID: 1334829446
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X < Y)
+            cout << "BIKE" << endl;
+        else if (Y < X)
+            cout << "CAR" << endl;
+        else
+            cout << "SAME" << endl;
+    }
+
+    return 0;
+}
