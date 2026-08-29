a57a4f545396f41ca4e1ac3ba35678cfa212cf2d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:49 +0530

    Add solution for Construct N (CONN)

---
+++ Construct N.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Construct N
+ * Problem Code: CONN
+ * Problem URL: https://www.codechef.com/problems/CONN
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Construct N (CONN)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
