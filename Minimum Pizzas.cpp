93599bb29411f34c61151a774ba519b5971b5afb
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:28 +0530

    Add solution for Minimum Pizzas (MINPIZZA)

---
+++ Minimum Pizzas.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Minimum Pizzas
+ * Problem Code: MINPIZZA
+ * Problem URL: https://www.codechef.com/problems/MINPIZZA
+ * Submission ID: 1325598114
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        cout << (N * X + 3) / 4 << endl;
+    }
+
+    return 0;
+}
