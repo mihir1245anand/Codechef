a72c754daef1bb74ac71c0b6569e479f7bdc6ef6
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:02 +0530

    Add solution for The Three Topics (THREETOPICS)

---
+++ The Three Topics.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: The Three Topics
+ * Problem Code: THREETOPICS
+ * Problem URL: https://www.codechef.com/problems/THREETOPICS
+ * Submission ID: 1338395786
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C, X;
+    cin >> A >> B >> C >> X;
+
+    if (X == A || X == B || X == C)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
