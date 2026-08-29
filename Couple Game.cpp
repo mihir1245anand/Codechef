79fdcdd458189f002f36150482945377e73ae6d2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:51 +0530

    Add solution for Couple Game (COUGAME)

---
+++ Couple Game.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Couple Game
+ * Problem Code: COUGAME
+ * Problem URL: https://www.codechef.com/problems/COUGAME
+ * Submission ID: 1330642854
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int G, B;
+        cin >> G >> B;
+
+        cout << B - G << endl;
+    }
+
+    return 0;
+}
