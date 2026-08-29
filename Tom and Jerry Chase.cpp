450dfc155f0f82b3c22ef319ded99dde00c61e59
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:02 +0530

    Add solution for Tom and Jerry Chase (JERRYCHASE)

---
+++ Tom and Jerry Chase.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Tom and Jerry Chase
+ * Problem Code: JERRYCHASE
+ * Problem URL: https://www.codechef.com/problems/JERRYCHASE
+ * Submission ID: 1330625534
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (Y > X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
