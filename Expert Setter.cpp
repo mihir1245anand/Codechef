a052cece979316f06661dc34e770f9c828a16e7b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:03 +0530

    Add solution for Expert Setter (EXPERT)

---
+++ Expert Setter.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Expert Setter
+ * Problem Code: EXPERT
+ * Problem URL: https://www.codechef.com/problems/EXPERT
+ * Submission ID: 1325599036
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long X, Y;
+        cin >> X >> Y;
+
+        if (2 * Y >= X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
