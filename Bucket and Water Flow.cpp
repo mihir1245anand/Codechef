5f55ce38fe086d616ceb970c4089b0ff9a0c3980
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:31 +0530

    Add solution for Bucket and Water Flow (WATERFLOW)

---
+++ Bucket and Water Flow.cpp
@@ -0,0 +1,31 @@
+/**
+ * Problem Name: Bucket and Water Flow
+ * Problem Code: WATERFLOW
+ * Problem URL: https://www.codechef.com/problems/WATERFLOW
+ * Submission ID: 1331113920
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int W, X, Y, Z;
+        cin >> W >> X >> Y >> Z;
+
+        int total = W + Y * Z;
+
+        if (total > X)
+            cout << "overflow" << endl;
+        else if (total == X)
+            cout << "filled" << endl;
+        else
+            cout << "unfilled" << endl;
+    }
+
+    return 0;
+}
