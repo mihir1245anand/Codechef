2737dd3cc082f68d35fb4fcaad6b7847347e69d8
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:26 +0530

    Add solution for Minimum number of coins (MINCOINS)

---
+++ Minimum number of coins.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Minimum number of coins
+ * Problem Code: MINCOINS
+ * Problem URL: https://www.codechef.com/problems/MINCOINS
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Minimum number of coins (MINCOINS)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
