288d36bf55f5857b94437b01d9a1670e288a6227
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:47 +0530

    Add solution for Right There (RIGHTTHERE)

---
+++ Right There.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Right There
+ * Problem Code: RIGHTTHERE
+ * Problem URL: https://www.codechef.com/problems/RIGHTTHERE
+ * Submission ID: 1330626269
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        if (X >= N)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
