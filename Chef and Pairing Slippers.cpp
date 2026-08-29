c11877dbcf5ccdffc5bcd2a7becb2cb4f44ed9e0
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:41 +0530

    Add solution for Chef and Pairing Slippers (CHEFSLP)

---
+++ Chef and Pairing Slippers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and Pairing Slippers
+ * Problem Code: CHEFSLP
+ * Problem URL: https://www.codechef.com/problems/CHEFSLP
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef and Pairing Slippers (CHEFSLP)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
