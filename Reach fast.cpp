12f211954bd251aaf81200f9c3283039bfe51851
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:43 +0530

    Add solution for Reach fast (REACHFAST)

---
+++ Reach fast.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Reach fast
+ * Problem Code: REACHFAST
+ * Problem URL: https://www.codechef.com/problems/REACHFAST
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Reach fast (REACHFAST)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
