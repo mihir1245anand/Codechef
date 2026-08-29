f4135471c811a59e638cbbc31e3ec48ec6e297f1
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:08 +0530

    Add solution for Four Tickets (FOURTICKETS)

---
+++ Four Tickets.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Four Tickets
+ * Problem Code: FOURTICKETS
+ * Problem URL: https://www.codechef.com/problems/FOURTICKETS
+ * Submission ID: 1330629819
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (4 * X <= 1000)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
