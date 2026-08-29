da6e07217f858730c608d5d568c255c5ec8f1af4
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:34 +0530

    Add solution for Candy Division (CANDIVIDE)

---
+++ Candy Division.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Name: Candy Division
+ * Problem Code: CANDIVIDE
+ * Problem URL: https://www.codechef.com/problems/CANDIVIDE
+ * Submission ID: 1330619655
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        if (N % 3 == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
