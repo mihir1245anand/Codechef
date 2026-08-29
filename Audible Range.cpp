c3b3d83def064ddbfa23e0bb98028c128e744093
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:22 +0530

    Add solution for Audible Range (AUDIBLE)

---
+++ Audible Range.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Audible Range
+ * Problem Code: AUDIBLE
+ * Problem URL: https://www.codechef.com/problems/AUDIBLE
+ * Submission ID: 1329308594
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X >= 67 && X <= 45000)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
