f73251659f34965389dd66a9b0bcb62093182962
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:22 +0530

    Add solution for ATM (HS08TEST)

---
+++ ATM.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: ATM
+ * Problem Code: HS08TEST
+ * Problem URL: https://www.codechef.com/problems/HS08TEST
+ * Submission ID: 1330671695
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    double Y;
+
+    cin >> X >> Y;
+
+    if (X % 5 == 0 && Y >= X + 0.50) {
+        Y = Y - X - 0.50;
+    }
+
+    cout << fixed << setprecision(2) << Y;
+
+    return 0;
+}
