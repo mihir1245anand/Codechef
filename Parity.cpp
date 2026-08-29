44cf6c196cbbb49ca81d280757077402ad0c86b1
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:34 +0530

    Add solution for Parity (PAR2)

---
+++ Parity.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Name: Parity
+ * Problem Code: PAR2
+ * Problem URL: https://www.codechef.com/problems/PAR2
+ * Submission ID: 1330621849
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        if (N % 2 == 0)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
+    return 0;
+}
