ed41124e865f8a0d9c89c88375de737da65f51b1
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:53 +0530

    Add solution for Credit score (CREDSCORE)

---
+++ Credit score.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: Credit score
+ * Problem Code: CREDSCORE
+ * Problem URL: https://www.codechef.com/problems/CREDSCORE
+ * Submission ID: 1331103617
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X >= 750)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
