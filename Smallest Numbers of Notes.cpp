00f2e5b1765c8ab9c991f7704af0d2b3c216dc65
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:53 +0530

    Add solution for Smallest Numbers of Notes (FLOW005)

---
+++ Smallest Numbers of Notes.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Smallest Numbers of Notes
+ * Problem Code: FLOW005
+ * Problem URL: https://www.codechef.com/problems/FLOW005
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Smallest Numbers of Notes (FLOW005)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
