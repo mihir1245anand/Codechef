64e092c5800a2b43e748bf59f7117f3815f1c91e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:04 +0530

    Add solution for Total Prize Money (PRIZEPOOL)

---
+++ Total Prize Money.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Total Prize Money
+ * Problem Code: PRIZEPOOL
+ * Problem URL: https://www.codechef.com/problems/PRIZEPOOL
+ * Submission ID: 1330622196
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << 10 * X + 90 * Y << endl;
+    }
+
+    return 0;
+}
