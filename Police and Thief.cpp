11e3e2cfbd248e76ee5375595f5e147628c468aa
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:38 +0530

    Add solution for Police and Thief (POLTHIEF)

---
+++ Police and Thief.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Police and Thief
+ * Problem Code: POLTHIEF
+ * Problem URL: https://www.codechef.com/problems/POLTHIEF
+ * Submission ID: 1346293712
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << abs(X - Y) << '\n';
+    }
+
+    return 0;
+}
