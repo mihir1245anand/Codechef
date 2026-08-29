b36a8ef9ea39a50389ed0b9ec5e0609387bb7bb3
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:30 +0530

    Add solution for Mutated Minions (CHN15A)

---
+++ Mutated Minions.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Mutated Minions
+ * Problem Code: CHN15A
+ * Problem URL: https://www.codechef.com/problems/CHN15A
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Mutated Minions (CHN15A)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
