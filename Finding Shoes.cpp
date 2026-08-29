e60a81efe03a61c93e6a1747e0094e554c019389
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:07 +0530

    Add solution for Finding Shoes (FINDSHOES)

---
+++ Finding Shoes.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Finding Shoes
+ * Problem Code: FINDSHOES
+ * Problem URL: https://www.codechef.com/problems/FINDSHOES
+ * Submission ID: 1348507599
+ * Language: C++
+ */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        int extraShoes = N + max(0, N - M);
+
+        cout << extraShoes << endl;
+    }
+
+    return 0;
+}
