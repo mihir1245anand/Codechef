0a6df1a5c442a82849e7b5f7a8c3bda929645f6c
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:13 +0530

    Add solution for How many unattempted problems (PRACLIST)

---
+++ How many unattempted problems.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: How many unattempted problems
+ * Problem Code: PRACLIST
+ * Problem URL: https://www.codechef.com/problems/PRACLIST
+ * Submission ID: 1325587422
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    cout << X - Y << endl;
+    return 0;
+}
