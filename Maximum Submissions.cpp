b82aebc3ec0af732d98da54c1b93a95e168f0497
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:24 +0530

    Add solution for Maximum Submissions (MAXIMUMSUBS)

---
+++ Maximum Submissions.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Maximum Submissions
+ * Problem Code: MAXIMUMSUBS
+ * Problem URL: https://www.codechef.com/problems/MAXIMUMSUBS
+ * Submission ID: 1330679015
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << (X * 60 - 5) / 30 + 1 << endl;
+    }
+
+    return 0;
+}
