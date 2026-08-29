b2400b063dc2549e6984b54687af67f5eb5f59e5
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:37 +0530

    Add solution for Penalty Shots (PENALTY)

---
+++ Penalty Shots.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Penalty Shots
+ * Problem Code: PENALTY
+ * Problem URL: https://www.codechef.com/problems/PENALTY
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Penalty Shots (PENALTY)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
