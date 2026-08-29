1a671c457f1832327c8bf09e8f092adb0495f1ba
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:44 +0530

    Add solution for Chef gives Party (PARTY2)

---
+++ Chef gives Party.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef gives Party
+ * Problem Code: PARTY2
+ * Problem URL: https://www.codechef.com/problems/PARTY2
+ * Submission ID: 1330659971
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X, K;
+        cin >> N >> X >> K;
+
+        if (K >= N * X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
