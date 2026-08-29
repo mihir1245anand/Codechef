9d9fd95663bfeafc03f091cdc64b332ca190aa31
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:07 +0530

    Add solution for Finding Square Roots (FSQRT)

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Finding Square Roots
+ * Problem Code: FSQRT
+ * Problem URL: https://www.codechef.com/problems/FSQRT
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Finding Square Roots (FSQRT)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
