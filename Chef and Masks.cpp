464529d847b2d040feaeff25390eb29d298dd4a7
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:40 +0530

    Add solution for Chef and Masks (CMASKS)

---
+++ Chef and Masks.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and Masks
+ * Problem Code: CMASKS
+ * Problem URL: https://www.codechef.com/problems/CMASKS
+ * Submission ID: 1330677111
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (10 * Y <= 100 * X)
+            cout << "CLOTH" << endl;
+        else
+            cout << "DISPOSABLE" << endl;
+    }
+
+    return 0;
+}
