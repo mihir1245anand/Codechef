64940e6de25b36aae55b17ad5a125aa283e0854c
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:18 +0530

    Add solution for 404 Not Found (ERROR404)

---
+++ 404 Not Found.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: 404 Not Found
+ * Problem Code: ERROR404
+ * Problem URL: https://www.codechef.com/problems/ERROR404
+ * Submission ID: 1325587770
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X == 404)
+        cout << "NOT FOUND" << endl;
+    else
+        cout << "FOUND" << endl;
+
+    return 0;
+}
