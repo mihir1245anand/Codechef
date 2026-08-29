ba14263d311a197ed376adb34865b387d4cb44f2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:38 +0530

    Add solution for Possible Victory (T20MCH)

---
+++ Possible Victory.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Possible Victory
+ * Problem Code: T20MCH
+ * Problem URL: https://www.codechef.com/problems/T20MCH
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Possible Victory (T20MCH)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
