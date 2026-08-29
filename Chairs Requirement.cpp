1fda235d49675d34b4d9739b791bdb557ee7742c
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:36 +0530

    Add solution for Chairs Requirement (CHAIRS_)

---
+++ Chairs Requirement.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Chairs Requirement
+ * Problem Code: CHAIRS_
+ * Problem URL: https://www.codechef.com/problems/CHAIRS_
+ * Submission ID: 1330630257
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << max(0, X - Y) << endl;
+    }
+
+    return 0;
+}
