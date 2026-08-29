793ab5cb0379069517a0f7e3d7182f883fd9d9a0
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:57 +0530

    Add solution for SYN2V2 (SYN2V2)

---
+++ SYN2V2.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: SYN2V2
+ * Problem Code: SYN2V2
+ * Problem URL: https://www.codechef.com/problems/SYN2V2
+ * Submission ID: 1296568434
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Replace the _ (underscores) with the correct value
+  cout << "I love c++";
+
+}
