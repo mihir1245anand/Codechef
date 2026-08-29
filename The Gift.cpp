0eb7fdea3d538f834576bb655b958cecd46477d8
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:59 +0530

    Add solution for The Gift (CS2023_GIFT)

---
+++ The Gift.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: The Gift
+ * Problem Code: CS2023_GIFT
+ * Problem URL: https://www.codechef.com/problems/CS2023_GIFT
+ * Submission ID: 1330663983
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X, N, M;
+    cin >> X >> N >> M;
+
+    if (X + M >= N)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
