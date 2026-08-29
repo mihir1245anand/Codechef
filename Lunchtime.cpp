b099cc16cec5cdfdae7a1185fb75bc0a2cdfa531
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:20 +0530

    Add solution for Lunchtime (LTIME)

---
+++ Lunchtime.cpp
@@ -0,0 +1,26 @@
+/**
+ * Problem Name: Lunchtime
+ * Problem Code: LTIME
+ * Problem URL: https://www.codechef.com/problems/LTIME
+ * Submission ID: 1330657467
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int X;
+        cin >> X;
+
+        if (X >= 1 && X <= 4)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl; 
+    }
+
+}
