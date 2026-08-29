5e695ffcbbb6d9468c312c0255ce9d0f3bb18f1d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:31 +0530

    Add solution for Bull or Bear (BULLBEAR)

---
+++ Bull or Bear.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Bull or Bear
+ * Problem Code: BULLBEAR
+ * Problem URL: https://www.codechef.com/problems/BULLBEAR
+ * Submission ID: 1330628687
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (Y > X)
+            cout << "PROFIT" << endl;
+        else if (Y < X)
+            cout << "LOSS" << endl;
+        else
+            cout << "NEUTRAL" << endl;
+    }
+
+    return 0;
+}
