20f4728934190170298554b2fa2db143f0fe764f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:27 +0530

    Add solution for Minimum number of Flips (MINFLIPS)

---
+++ Minimum number of Flips.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Minimum number of Flips
+ * Problem Code: MINFLIPS
+ * Problem URL: https://www.codechef.com/problems/MINFLIPS
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Minimum number of Flips (MINFLIPS)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
