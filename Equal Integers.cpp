2b7a14637ab41d90d004e182e93935ea6dfaf1a8
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:01 +0530

    Add solution for Equal Integers (INCREAR)

---
+++ Equal Integers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Equal Integers
+ * Problem Code: INCREAR
+ * Problem URL: https://www.codechef.com/problems/INCREAR
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Equal Integers (INCREAR)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
