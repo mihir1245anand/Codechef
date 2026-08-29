571b19ea6eee8dee425c3834782ac91ea39ba062
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:57 +0530

    Add solution for Discus Throw (DISCUS)

---
+++ Discus Throw.cpp
@@ -0,0 +1,33 @@
+/**
+ * Problem Name: Discus Throw
+ * Problem Code: DISCUS
+ * Problem URL: https://www.codechef.com/problems/DISCUS
+ * Submission ID: 1345152088
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int x,y,z;
+		cin>>x>>y>>z;
+		if (x>=y && x>=z){
+			cout<<x<<endl;
+		}
+		else if(y>=x && y>=z){
+			cout<<y<<endl;
+		}
+		else{
+			cout<<z<<endl;
+		}
+	}
+	
+	
+	
+	
+	return 0;
+}
