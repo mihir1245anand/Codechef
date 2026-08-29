23439045ebf4bc3f85a2b13df5aac45760c78329
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:44 +0530

    Add solution for Reach the Target (REACHTARGET)

---
+++ Reach the Target.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Reach the Target
+ * Problem Code: REACHTARGET
+ * Problem URL: https://www.codechef.com/problems/REACHTARGET
+ * Submission ID: 1330584166
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+	cin>>T;
+	
+	while(T--){
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x-y<<endl;
+	}
+	
+
+}
