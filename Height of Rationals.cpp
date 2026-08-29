3deb57f59efefa4979eadf3d0cfc35950303924e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:12 +0530

    Add solution for Height of Rationals (HEIGHTRATION)

---
+++ Height of Rationals.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Name: Height of Rationals
+ * Problem Code: HEIGHTRATION
+ * Problem URL: https://www.codechef.com/problems/HEIGHTRATION
+ * Submission ID: 1330670398
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << max(a, b);
+
+    return 0;
+}
