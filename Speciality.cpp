5805d2fcb188a5fc252bd11edc490d68af2759f9
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:54 +0530

    Add solution for Speciality (SPECIALITY)

---
+++ Speciality.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Speciality
+ * Problem Code: SPECIALITY
+ * Problem URL: https://www.codechef.com/problems/SPECIALITY
+ * Submission ID: 1330677936
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        if (X > Y && X > Z)
+            cout << "Setter" << endl;
+        else if (Y > X && Y > Z)
+            cout << "Tester" << endl;
+        else
+            cout << "Editorialist" << endl;
+    }
+
+    return 0;
+}
