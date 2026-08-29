1ef381e4ee23fbc3b07275fa45489511ccda337d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:38 +0530

    Add solution for Chef and Chapters (SEMCOURSES)

---
+++ Chef and Chapters.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Chef and Chapters
+ * Problem Code: SEMCOURSES
+ * Problem URL: https://www.codechef.com/problems/SEMCOURSES
+ * Submission ID: 1330654196
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        cout << X * Y * Z << endl;
+    }
+
+    return 0;
+}
