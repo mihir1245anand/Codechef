194146aa9bbd661ad249726b6b8925e157e69cea
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:48 +0530

    Add solution for Sasta Shark Tank (SST)

---
+++ Sasta Shark Tank.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Sasta Shark Tank
+ * Problem Code: SST
+ * Problem URL: https://www.codechef.com/problems/SST
+ * Submission ID: 1343402531
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B;
+        cin >> A >> B;
+
+        if (2 * A > B)
+            cout << "FIRST\n";
+        else if (2 * A < B)
+            cout << "SECOND\n";
+        else
+            cout << "ANY\n";
+    }
+
+    return 0;
+}
