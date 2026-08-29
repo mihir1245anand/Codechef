9d2aa287c94382c3bd8491c4268ed3e16bc135a2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:20 +0530

    Add solution for Air Conditioner Temperature (ACTEMP)

---
+++ Air Conditioner Temperature.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Name: Air Conditioner Temperature
+ * Problem Code: ACTEMP
+ * Problem URL: https://www.codechef.com/problems/ACTEMP
+ * Submission ID: 1343019005
+ * Language: C++
+ */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        if (max(A, C) <= B)
+            cout << "Yes\n";
+        else
+            cout << "No\n";
+    }
+
+    return 0;
+}
