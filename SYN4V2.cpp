0a9d1c8b4abcf1d6546ed80eb1464b1b86875dab
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:57 +0530

    Add solution for SYN4V2 (SYN4V2)

---
+++ SYN4V2.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: SYN4V2
+ * Problem Code: SYN4V2
+ * Problem URL: https://www.codechef.com/problems/SYN4V2
+ * Submission ID: 1296569138
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // add two cout statements
+cout<<3+4;
+cout<<2+1;
+
+}
