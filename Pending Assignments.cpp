ad73e0238f66bb935c94d44e03e23aaa829c9d90
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:37 +0530

    Add solution for Pending Assignments (ASSIGNMNT)

---
+++ Pending Assignments.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Pending Assignments
+ * Problem Code: ASSIGNMNT
+ * Problem URL: https://www.codechef.com/problems/ASSIGNMNT
+ * Submission ID: 1331106138
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        if (X * Y <= Z * 1440)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
