b44f6759249d3ad0a9ea9ed3ef1bf949ed994cda
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:07 +0530

    Add solution for Utkarsh and Placement tests (UTKPLC)

---
+++ Utkarsh and Placement tests.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Utkarsh and Placement tests
+ * Problem Code: UTKPLC
+ * Problem URL: https://www.codechef.com/problems/UTKPLC
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Utkarsh and Placement tests (UTKPLC)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
