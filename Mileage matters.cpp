e47856ed2a1575ecebd3c9dfb9c6de172b7cafd9
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:25 +0530

    Add solution for Mileage matters (MILEAGE)

---
+++ Mileage matters.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Mileage matters
+ * Problem Code: MILEAGE
+ * Problem URL: https://www.codechef.com/problems/MILEAGE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Mileage matters (MILEAGE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
