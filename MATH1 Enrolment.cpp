5b1f792239398f26432723f3f79cbf6c95916121
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:22 +0530

    Add solution for MATH1 Enrolment (M1ENROL)

---
+++ MATH1 Enrolment.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: MATH1 Enrolment
+ * Problem Code: M1ENROL
+ * Problem URL: https://www.codechef.com/problems/M1ENROL
+ * Submission ID: 1330648485
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << max(0, Y - X) << endl;
+    }
+
+    return 0;
+}
