ba25684eb302062f81bdd35f5257380e8acf9831
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:26 +0530

    Add solution for Best of N Sets (BESTOFTENNIS)

---
+++ Best of N Sets.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Best of N Sets
+ * Problem Code: BESTOFTENNIS
+ * Problem URL: https://www.codechef.com/problems/BESTOFTENNIS
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Best of N Sets (BESTOFTENNIS)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
