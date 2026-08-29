b0115b82ed723e9832e0e4f9a2ecfdd22bfd0539
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:08 +0530

    Add solution for Flip the cards (FLIPCARDS)

---
+++ Flip the cards.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Flip the cards
+ * Problem Code: FLIPCARDS
+ * Problem URL: https://www.codechef.com/problems/FLIPCARDS
+ * Submission ID: 1346983646
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        cout << min(X, N - X) << '\n';
+    }
+
+    return 0;
+}
