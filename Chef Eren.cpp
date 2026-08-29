7f65fbb235fc0a45e7a965985726a02615b57d23
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:44 +0530

    Add solution for Chef Eren (CHEFEREN)

---
+++ Chef Eren.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef Eren
+ * Problem Code: CHEFEREN
+ * Problem URL: https://www.codechef.com/problems/CHEFEREN
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef Eren (CHEFEREN)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
