2e36e376425b0db2d904f391f87cd4a798b6e916
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:06 +0530

    Add solution for Tyre problem (TYRE)

---
+++ Tyre problem.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Tyre problem
+ * Problem Code: TYRE
+ * Problem URL: https://www.codechef.com/problems/TYRE
+ * Submission ID: 1331101188
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int n,m;
+        cin>>n>>m;
+        cout<<n*2+m*4<<endl;
+        
+    }
+
+}
