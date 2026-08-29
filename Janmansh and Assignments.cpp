69225a29b1612622fd194e11b144de1bf9fb24a5
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:18 +0530

    Add solution for Janmansh and Assignments (JASSIGNMENTS)

---
+++ Janmansh and Assignments.cpp
@@ -0,0 +1,26 @@
+/**
+ * Problem Name: Janmansh and Assignments
+ * Problem Code: JASSIGNMENTS
+ * Problem URL: https://www.codechef.com/problems/JASSIGNMENTS
+ * Submission ID: 1332563257
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int x;
+	    cin>>x;
+	    
+	    if(x<=7)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+
+}
