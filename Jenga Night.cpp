17cae207fdf60a7286cde519c9eb1e685c8091a2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:18 +0530

    Add solution for Jenga Night (JENGA)

---
+++ Jenga Night.cpp
@@ -0,0 +1,30 @@
+/**
+ * Problem Name: Jenga Night
+ * Problem Code: JENGA
+ * Problem URL: https://www.codechef.com/problems/JENGA
+ * Submission ID: 1345083973
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int N,X;
+	    cin>>N>>X;
+	    if(X%N==0){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+	
+
+}
