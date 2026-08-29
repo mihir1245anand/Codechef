0f1e2c1d40035b89b35ad2d40cf0f5713f9a5878
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:30 +0530

    Add solution for Multivitamin Tablets (TABLETS)

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Multivitamin Tablets
+ * Problem Code: TABLETS
+ * Problem URL: https://www.codechef.com/problems/TABLETS
+ * Submission ID: 1330661582
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (Y >= 3 * X)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
