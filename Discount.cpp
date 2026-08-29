f042ebb1aa204da1109defc045d4742daef31b54
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:56 +0530

    Add solution for Discount (DISCNT)

---
+++ Discount.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Discount
+ * Problem Code: DISCNT
+ * Problem URL: https://www.codechef.com/problems/DISCNT
+ * Submission ID: 1330670276
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int x;
+        cin >> x;
+
+        cout << 100 - x << endl;
+    }
+
+    return 0;
+}
