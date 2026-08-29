16476624e0d124bd0002736978b0955ecb5423c8
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:42 +0530

    Add solution for Chef and Races (CHEFRACES)

---
+++ Chef and Races.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and Races
+ * Problem Code: CHEFRACES
+ * Problem URL: https://www.codechef.com/problems/CHEFRACES
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef and Races (CHEFRACES)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
