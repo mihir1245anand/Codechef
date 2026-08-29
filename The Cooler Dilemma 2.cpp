3e1f81c6eda21540287ffc2226ad0d0d3ebe43a2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:59 +0530

    Add solution for The Cooler Dilemma 2 (WATERCOOLER2)

---
+++ The Cooler Dilemma 2.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: The Cooler Dilemma 2
+ * Problem Code: WATERCOOLER2
+ * Problem URL: https://www.codechef.com/problems/WATERCOOLER2
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: The Cooler Dilemma 2 (WATERCOOLER2)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
