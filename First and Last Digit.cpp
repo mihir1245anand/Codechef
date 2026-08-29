bf993fa4e0ab6568b1a8a30299fd4ac213c23a86
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:07 +0530

    Add solution for First and Last Digit (FLOW004)

---
+++ First and Last Digit.cpp
@@ -0,0 +1,31 @@
+/**
+ * Problem Name: First and Last Digit
+ * Problem Code: FLOW004
+ * Problem URL: https://www.codechef.com/problems/FLOW004
+ * Submission ID: 1331104052
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        int last = N % 10;
+        int first = N;
+
+        while (first >= 10) {
+            first /= 10;
+        }
+
+        cout << first + last << endl;
+    }
+
+    return 0;
+}
