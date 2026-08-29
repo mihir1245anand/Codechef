b8aacdaff983efe8298add2c869c1a9e6d44bd94
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:34 +0530

    Add solution for Car Choice (CARCHOICE)

---
+++ Car Choice.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Car Choice
+ * Problem Code: CARCHOICE
+ * Problem URL: https://www.codechef.com/problems/CARCHOICE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Car Choice (CARCHOICE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
