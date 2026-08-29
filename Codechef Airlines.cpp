33a5bf8190dfb47b72d39b77161befe06884015b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:49 +0530

    Add solution for Codechef Airlines (AIRLINES)

---
+++ Codechef Airlines.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Codechef Airlines
+ * Problem Code: AIRLINES
+ * Problem URL: https://www.codechef.com/problems/AIRLINES
+ * Submission ID: 1331110204
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        int seats = 10 * X;
+        cout << min(Y, seats) * Z << endl;
+    }
+
+    return 0;
+}
