d7e784c40dd5ffa0ff705a4996aada4d2a38e0f1
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:59 +0530

    Add solution for Drunk Alcoholic (DRUNKALK)

---
+++ Drunk Alcoholic.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Drunk Alcoholic
+ * Problem Code: DRUNKALK
+ * Problem URL: https://www.codechef.com/problems/DRUNKALK
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Drunk Alcoholic (DRUNKALK)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
