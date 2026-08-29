5945084083e48ade6ebeb787184cc871ae1dfc0b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:28 +0530

    Add solution for Binary Battles (BIN_BAT)

---
+++ Binary Battles.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Binary Battles
+ * Problem Code: BIN_BAT
+ * Problem URL: https://www.codechef.com/problems/BIN_BAT
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Binary Battles (BIN_BAT)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
