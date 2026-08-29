f62efef442163cae2c4a4448532c0ecc45d64ce5
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:41 +0530

    Add solution for Product Exceed (PRDEXC)

---
+++ Product Exceed.cpp
@@ -0,0 +1,39 @@
+/**
+ * Problem Name: Product Exceed
+ * Problem Code: PRDEXC
+ * Problem URL: https://www.codechef.com/problems/PRDEXC
+ * Submission ID: 1322301499
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, P;
+        cin >> X >> Y >> P;
+
+        if (X * Y >= P) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        int ans = 1000000;
+
+        // Try all possible increments of X
+        for (int a = 0; a <= 100; a++) {
+            int nx = X + a;
+            int b = 0;
+            while (nx * (Y + b) < P) b++;
+            ans = min(ans, a + b);
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
