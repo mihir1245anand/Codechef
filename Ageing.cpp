a7f58e79ca60adc3dc92c9492b6e856a8c85a659
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:19 +0530

    Add solution for Ageing (AGEING)

---
+++ Ageing.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Ageing
+ * Problem Code: AGEING
+ * Problem URL: https://www.codechef.com/problems/AGEING
+ * Submission ID: 1330625926
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << X - 10 << endl;
+    }
+
+    return 0;
+}
