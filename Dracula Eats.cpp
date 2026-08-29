f14d9361e0311564195a00d327eb9a250fc6f217
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:59 +0530

    Add solution for Dracula Eats (CHEAT)

---
+++ Dracula Eats.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Dracula Eats
+ * Problem Code: CHEAT
+ * Problem URL: https://www.codechef.com/problems/CHEAT
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Dracula Eats (CHEAT)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
