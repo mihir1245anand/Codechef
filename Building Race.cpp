7f2d79cf640249ef1aec555601c6a4a96277daa5
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:31 +0530

    Add solution for Building Race (BUILDINGRACE)

---
+++ Building Race.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Building Race
+ * Problem Code: BUILDINGRACE
+ * Problem URL: https://www.codechef.com/problems/BUILDINGRACE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Building Race (BUILDINGRACE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
