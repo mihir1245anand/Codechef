2244782b710a7a7cc43afd9bc5045c5f43ed7cd6
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:22 +0530

    Add solution for Masterchef finals (TOP10)

---
+++ Masterchef finals.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Masterchef finals
+ * Problem Code: TOP10
+ * Problem URL: https://www.codechef.com/problems/TOP10
+ * Submission ID: 1325586205
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X <= 10)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
