1ee46d6d67b496d9679bca1437bb690128db2c1e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:58 +0530

    Add solution for Test Score (CHEFSCORE)

---
+++ Test Score.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Test Score
+ * Problem Code: CHEFSCORE
+ * Problem URL: https://www.codechef.com/problems/CHEFSCORE
+ * Submission ID: 1344282734
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X, Y;
+        cin >> N >> X >> Y;
+
+        if (Y % X == 0 && Y <= N * X)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
