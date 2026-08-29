57f6cfb58c698e25f29c1bd9b9f33352390b8080
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:30 +0530

    Add solution for Bone Appetit (BNE_APT)

---
+++ Bone Appetit.cpp
@@ -0,0 +1,20 @@
+/**
+ * Problem Name: Bone Appetit
+ * Problem Code: BNE_APT
+ * Problem URL: https://www.codechef.com/problems/BNE_APT
+ * Submission ID: 1329311489
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, M, X, Y;
+    cin >> N >> M;
+    cin >> X >> Y;
+
+    cout << N * X + M * Y;
+
+    return 0;
+}
