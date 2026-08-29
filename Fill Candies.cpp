3ab6fdb0f627f510d5f0a63c94e643fe53ce6c54
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:05 +0530

    Add solution for Fill Candies (FILLCANDIES)

---
+++ Fill Candies.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Fill Candies
+ * Problem Code: FILLCANDIES
+ * Problem URL: https://www.codechef.com/problems/FILLCANDIES
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Fill Candies (FILLCANDIES)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
