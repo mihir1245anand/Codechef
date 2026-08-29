9e27758f56bc51e0a681778a6e9b8991bdff43be
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:43 +0530

    Add solution for Chef and Water Bottles (CHEFBOTTLE)

---
+++ Chef and Water Bottles.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and Water Bottles
+ * Problem Code: CHEFBOTTLE
+ * Problem URL: https://www.codechef.com/problems/CHEFBOTTLE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef and Water Bottles (CHEFBOTTLE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
