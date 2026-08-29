f409c4a12468a5473e7e4ca39c729128614b0378
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:51 +0530

    Add solution for Single-use Attack (SINGLEUSE)

---
+++ Single-use Attack.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Single-use Attack
+ * Problem Code: SINGLEUSE
+ * Problem URL: https://www.codechef.com/problems/SINGLEUSE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Single-use Attack (SINGLEUSE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
