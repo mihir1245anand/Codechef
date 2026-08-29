7c01ea3f87d08ac4cbbc9e3ead98c0a40702bdd7
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:39 +0530

    Add solution for Chef and Donation (DNATION)

---
+++ Chef and Donation.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Chef and Donation
+ * Problem Code: DNATION
+ * Problem URL: https://www.codechef.com/problems/DNATION
+ * Submission ID: 1330630843
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << Y - X << endl;
+    }
+
+    return 0;
+}
