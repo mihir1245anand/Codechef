26d190248eeca3a437427da590df82a97aae3a38
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:05 +0530

    Add solution for Fill the Bucket (FBC)

---
+++ Fill the Bucket.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Fill the Bucket
+ * Problem Code: FBC
+ * Problem URL: https://www.codechef.com/problems/FBC
+ * Submission ID: 1330673213
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int K, X;
+        cin >> K >> X;
+
+        cout << K - X << endl;
+    }
+
+    return 0;
+}
