c0dcacc62c6cf86fc8fa6bd2eed82ab60584610b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:23 +0530

    Add solution for Maximum Lead (MXLED)

---
+++ Maximum Lead.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Maximum Lead
+ * Problem Code: MXLED
+ * Problem URL: https://www.codechef.com/DSAMONDAY015/problems/MXLED
+ * Contest: DSAMONDAY015
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            // Problem: Maximum Lead (MXLED)
+        }
+    }
+    return 0;
+}
