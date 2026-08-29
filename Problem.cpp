d1860eb05eabbe28f690305a0f4f389206b649a7
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:40 +0530

    Add solution for Problem (PR0BLEM)

---
+++ Problem.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Problem
+ * Problem Code: PR0BLEM
+ * Problem URL: https://www.codechef.com/problems/PR0BLEM
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Problem (PR0BLEM)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
