576e2c41f81b06d44a81e779d4f2b53cc13e3384
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:29 +0530

    Add solution for Monthly Budget (BUDGET_)

---
+++ Monthly Budget.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Monthly Budget
+ * Problem Code: BUDGET_
+ * Problem URL: https://www.codechef.com/problems/BUDGET_
+ * Submission ID: 1331102543
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X >= 30 * Y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
