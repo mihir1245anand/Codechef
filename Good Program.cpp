d19a058638f481278866cb65ec47e8ab4782cecc
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:10 +0530

    Add solution for Good Program (NIBBLE)

---
+++ Good Program.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Good Program
+ * Problem Code: NIBBLE
+ * Problem URL: https://www.codechef.com/problems/NIBBLE
+ * Submission ID: 1343831806
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        if (N % 4 == 0)
+            cout << "Good\n";
+        else
+            cout << "Not Good\n";
+    }
+
+    return 0;
+}
