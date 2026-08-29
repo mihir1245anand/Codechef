ed66f7934420c4df1df7392c27d03ec680496a2b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:17 +0530

    Add solution for Is the Score Consistent (TRUESCORE)

---
+++ Is the Score Consistent.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Name: Is the Score Consistent
+ * Problem Code: TRUESCORE
+ * Problem URL: https://www.codechef.com/problems/TRUESCORE
+ * Submission ID: 1336573313
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C, D;
+        cin >> A >> B;
+        cin >> C >> D;
+
+        if (C >= A && D >= B)
+            cout << "POSSIBLE" << endl;
+        else
+            cout << "IMPOSSIBLE" << endl;
+    }
+
+    return 0;
+}
