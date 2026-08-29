8a9cc2e1096f56544c0e5710943f80da3381c04e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:23 +0530

    Add solution for Badminton Serves (BADMINTON)

---
+++ Badminton Serves.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Badminton Serves
+ * Problem Code: BADMINTON
+ * Problem URL: https://www.codechef.com/problems/BADMINTON
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Badminton Serves (BADMINTON)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
