872622214f5d630951bc1fcabd2dd52fe7ba5397
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:29 +0530

    Add solution for Monopoly in Chefland (MONOPOLY)

---
+++ Monopoly in Chefland.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Monopoly in Chefland
+ * Problem Code: MONOPOLY
+ * Problem URL: https://www.codechef.com/problems/MONOPOLY
+ * Submission ID: 1331112907
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int R1, R2, R3;
+        cin >> R1 >> R2 >> R3;
+
+        if (R1 > R2 + R3 || R2 > R1 + R3 || R3 > R1 + R2)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
