b23f239294b7094e478521718db358960a85c618
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:08 +0530

    Add solution for Varied Digits (VARDIGS)

---
+++ Varied Digits.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Varied Digits
+ * Problem Code: VARDIGS
+ * Problem URL: https://www.codechef.com/problems/VARDIGS
+ * Submission ID: 1322130983
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	int tens=x/10;
+	int ones=x%10;
+	
+	if(tens!=ones)
+	cout<<"yes";
+	else
+	cout<<"No";
+	return 0;
+	
+
+}
