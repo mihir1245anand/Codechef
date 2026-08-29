fddc621971ddc96c78697c77560faca111514f87
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:05 +0530

    Add solution for True and False Paper (TFPAPER)

---
+++ True and False Paper.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: True and False Paper
+ * Problem Code: TFPAPER
+ * Problem URL: https://www.codechef.com/problems/TFPAPER
+ * Submission ID: 1330668981
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        cout << N - K << endl;
+    }
+
+    return 0;
+}
