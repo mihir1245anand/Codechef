c6629868878f892faa3e2cade4ad34f0da9f29b6
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:14 +0530

    Add solution for Instagram (INSTAGRAM)

---
+++ Instagram.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Instagram
+ * Problem Code: INSTAGRAM
+ * Problem URL: https://www.codechef.com/problems/INSTAGRAM
+ * Submission ID: 1330670528
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X > 10 * Y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
