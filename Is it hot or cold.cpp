3378d3b39ea61418889e957e2b6d5b34eb4c91cf
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:16 +0530

    Add solution for Is it hot or cold (HOTCOLD)

---
+++ Is it hot or cold.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Is it hot or cold
+ * Problem Code: HOTCOLD
+ * Problem URL: https://www.codechef.com/problems/HOTCOLD
+ * Submission ID: 1330672449
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int C;
+        cin >> C;
+
+        if (C > 20)
+            cout << "HOT" << endl;
+        else
+            cout << "COLD" << endl;
+    }
+
+    return 0;
+}
