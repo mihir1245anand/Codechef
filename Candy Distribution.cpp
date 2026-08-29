879f1ba348ca1be9d6db1a3ce75832fb37299319
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:33 +0530

    Add solution for Candy Distribution (CANDYDIST)

---
+++ Candy Distribution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Candy Distribution
+ * Problem Code: CANDYDIST
+ * Problem URL: https://www.codechef.com/problems/CANDYDIST
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Candy Distribution (CANDYDIST)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
