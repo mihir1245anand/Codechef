efb00c3d6bebb6ef0858c2c3011e324d99df7c7e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:24 +0530

    Add solution for Bath in Winters (BATH)

---
+++ Bath in Winters.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Bath in Winters
+ * Problem Code: BATH
+ * Problem URL: https://www.codechef.com/problems/BATH
+ * Submission ID: 1347233078
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << X / (2 * Y) << endl;
+    }
+
+    return 0;
+}
