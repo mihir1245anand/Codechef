6f99dceffdc845d441e2469958b2c4cde3a4b684
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:55 +0530

    Add solution for Spice Level (KITCHENSPICE)

---
+++ Spice Level.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Spice Level
+ * Problem Code: KITCHENSPICE
+ * Problem URL: https://www.codechef.com/problems/KITCHENSPICE
+ * Submission ID: 1330663658
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X < 4)
+            cout << "MILD" << endl;
+        else if (X < 7)
+            cout << "MEDIUM" << endl;
+        else
+            cout << "HOT" << endl;
+    }
+
+    return 0;
+}
