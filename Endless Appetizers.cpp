27f73ae8a67c0829449b2a94b97fdc607686f950
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:00 +0530

    Add solution for Endless Appetizers (MOZZ)

---
+++ Endless Appetizers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Endless Appetizers
+ * Problem Code: MOZZ
+ * Problem URL: https://www.codechef.com/problems/MOZZ
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Endless Appetizers (MOZZ)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
