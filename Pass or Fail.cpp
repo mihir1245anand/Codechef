038f90e33260c5427cc5109c50aecfed43184bdb
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:35 +0530

    Add solution for Pass or Fail (PASSORFAIL)

---
+++ Pass or Fail.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Pass or Fail
+ * Problem Code: PASSORFAIL
+ * Problem URL: https://www.codechef.com/problems/PASSORFAIL
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Pass or Fail (PASSORFAIL)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
