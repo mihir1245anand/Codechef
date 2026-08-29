539e79f84bba72e51d2d07c5a027481018269558
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:45 +0530

    Add solution for Chef On Date (CHEFONDATE)

---
+++ Chef On Date.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef On Date
+ * Problem Code: CHEFONDATE
+ * Problem URL: https://www.codechef.com/problems/CHEFONDATE
+ * Submission ID: 1330620947
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X >= Y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
