e5b3a80956118f27ec2ffa033c7fd47567a80183
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:23 +0530

    Add solution for Maximise the Tastiness (MAXTASTE)

---
+++ Maximise the Tastiness.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Maximise the Tastiness
+ * Problem Code: MAXTASTE
+ * Problem URL: https://www.codechef.com/problems/MAXTASTE
+ * Submission ID: 1345556318
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << max(a, b) + max(c, d) << endl;
+    }
+
+    return 0;
+}
