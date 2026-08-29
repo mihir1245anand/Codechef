27541c2e5b181973ad72d058d61b5c5f5dc485db
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:05 +0530

    Add solution for Final Population (POPULATION)

---
+++ Final Population.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: Final Population
+ * Problem Code: POPULATION
+ * Problem URL: https://www.codechef.com/problems/POPULATION
+ * Submission ID: 1330659739
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        cout << X - Y + Z << endl;
+    }
+}
