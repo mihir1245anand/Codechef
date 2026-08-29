2db035671edc9f72ca8853c3277dd13ee9a33c8b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:19 +0530

    Add solution for Lucky Seven (LUCKYSEVEN)

---
+++ Lucky Seven.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: Lucky Seven
+ * Problem Code: LUCKYSEVEN
+ * Problem URL: https://www.codechef.com/problems/LUCKYSEVEN
+ * Submission ID: 1325583901
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    cout << S[6] << endl;
+    return 0;
+}
