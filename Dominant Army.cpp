225b1904d37cebba38ec1e1a962c8d078f9fe6b7
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:57 +0530

    Add solution for Dominant Army (DOMINANT)

---
+++ Dominant Army.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Dominant Army
+ * Problem Code: DOMINANT
+ * Problem URL: https://www.codechef.com/problems/DOMINANT
+ * Submission ID: 1331114523
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int NA, NB, NC;
+        cin >> NA >> NB >> NC;
+
+        if (NA > NB + NC || NB > NA + NC || NC > NA + NB)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
