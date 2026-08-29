995b3b5b291518406b83ae150e72e2c261b08e26
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:42 +0530

    Add solution for Puzzle Hunt (PUZHUNT)

---
+++ Puzzle Hunt.cpp
@@ -0,0 +1,22 @@
+/**
+ * Problem Name: Puzzle Hunt
+ * Problem Code: PUZHUNT
+ * Problem URL: https://www.codechef.com/problems/PUZHUNT
+ * Submission ID: 1329311201
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N >= 6 && N <= 8)
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
