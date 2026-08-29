29e5191f29068f928f94a9fa4db2858c401d12ad
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:11 +0530

    Add solution for Greater Average (AVGPROBLEM)

---
+++ Greater Average.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Greater Average
+ * Problem Code: AVGPROBLEM
+ * Problem URL: https://www.codechef.com/problems/AVGPROBLEM
+ * Submission ID: 1323448327
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        if (A + B > 2 * C)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
