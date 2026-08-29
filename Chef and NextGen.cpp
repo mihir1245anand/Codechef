c6b05c721508b7e7fe3f7fb7f3dec652cad461dc
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:41 +0530

    Add solution for Chef and NextGen (HELIUM3)

---
+++ Chef and NextGen.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and NextGen
+ * Problem Code: HELIUM3
+ * Problem URL: https://www.codechef.com/problems/HELIUM3
+ * Submission ID: 1332044135
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, X, Y;
+        cin >> A >> B >> X >> Y;
+
+        if (X * Y >= A * B)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
+    return 0;
+}
