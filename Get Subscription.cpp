1fedbf330171cbc13ac0b522c669bb10c1a39c33
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:09 +0530

    Add solution for Get Subscription (SUBSCRIBE)

---
+++ Get Subscription.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Get Subscription
+ * Problem Code: SUBSCRIBE
+ * Problem URL: https://www.codechef.com/problems/SUBSCRIBE
+ * Submission ID: 1330632800
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X > 30)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
