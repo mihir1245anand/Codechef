bbf55e66e1113f3c0f7215ddeefa0f99f5400709
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:47 +0530

    Add solution for Chessboard Distance (CHESSDIST)

---
+++ Chessboard Distance.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chessboard Distance
+ * Problem Code: CHESSDIST
+ * Problem URL: https://www.codechef.com/problems/CHESSDIST
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chessboard Distance (CHESSDIST)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
