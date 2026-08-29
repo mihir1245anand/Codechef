53e231470c6026e2a2aecd9ec0021a98ab3e89b4
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:21 +0530

    Add solution for Area OR Perimeter (AREAPERI)

---
+++ Area OR Perimeter.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Area OR Perimeter
+ * Problem Code: AREAPERI
+ * Problem URL: https://www.codechef.com/problems/AREAPERI
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Area OR Perimeter (AREAPERI)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
