923ad14032a8e5914a31c06624d3a77792f076a3
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:25 +0530

    Add solution for Battery Low (BATTERYLOW)

---
+++ Battery Low.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Battery Low
+ * Problem Code: BATTERYLOW
+ * Problem URL: https://www.codechef.com/problems/BATTERYLOW
+ * Submission ID: 1331110905
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X <= 15)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+
+    return 0;
+}
