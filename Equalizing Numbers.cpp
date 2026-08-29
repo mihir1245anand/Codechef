eb9fb4184226c3f7929f2a17e2867d1a2cd3b889
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:02 +0530

    Add solution for Equalizing Numbers (EQLZING)

---
+++ Equalizing Numbers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Equalizing Numbers
+ * Problem Code: EQLZING
+ * Problem URL: https://www.codechef.com/problems/EQLZING
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Equalizing Numbers (EQLZING)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
