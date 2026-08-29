b4e4c198198b7d0e30f5fb50cb540b6fe6737eb8
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:48 +0530

    Add solution for Saving Taxes (TAXSAVING)

---
+++ Saving Taxes.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Saving Taxes
+ * Problem Code: TAXSAVING
+ * Problem URL: https://www.codechef.com/problems/TAXSAVING
+ * Submission ID: 1325585401
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << X - Y << endl;
+    }
+
+    return 0;
+}
