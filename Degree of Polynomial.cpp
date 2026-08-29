8a03cc998311f9b6e21aebe9feeac942278b4e65
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:55 +0530

    Add solution for Degree of Polynomial (DPOLY)

---
+++ Degree of Polynomial.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Degree of Polynomial
+ * Problem Code: DPOLY
+ * Problem URL: https://www.codechef.com/problems/DPOLY
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Degree of Polynomial (DPOLY)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
