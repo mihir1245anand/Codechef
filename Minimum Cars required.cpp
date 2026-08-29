b269a9c3d08d3f1ce7296961577c3a7c3537ced3
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:25 +0530

    Add solution for Minimum Cars required (MINCARS)

---
+++ Minimum Cars required.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Minimum Cars required
+ * Problem Code: MINCARS
+ * Problem URL: https://www.codechef.com/problems/MINCARS
+ * Submission ID: 1343832882
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        cout << (N + 3) / 4 << endl;
+    }
+
+    return 0;
+}
