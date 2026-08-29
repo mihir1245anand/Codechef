81665c609cc4d4911cdab100662e7c6037c4da26
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:46 +0530

    Add solution for Chef Plays Ludo (LUDO)

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef Plays Ludo
+ * Problem Code: LUDO
+ * Problem URL: https://www.codechef.com/problems/LUDO
+ * Submission ID: 1325587350
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X == 6)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
