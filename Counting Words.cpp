7a2b295b39a7ac2e0e72ba84029220ea99798b6f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:51 +0530

    Add solution for Counting Words (CNTWRD)

---
+++ Counting Words.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: Counting Words
+ * Problem Code: CNTWRD
+ * Problem URL: https://www.codechef.com/problems/CNTWRD
+ * Submission ID: 1330623493
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+
+        cout << N * M << endl;
+    }
+
+    return 0;
+}
