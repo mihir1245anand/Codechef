5775e15359bb5ba5fb9c329a570715741cf5a792
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:08 +0530

    Add solution for Volume Control (VOLCONTROL)

---
+++ Volume Control.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Volume Control
+ * Problem Code: VOLCONTROL
+ * Problem URL: https://www.codechef.com/problems/VOLCONTROL
+ * Submission ID: 1330671170
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << abs(X - Y) << endl;
+    }
+
+    return 0;
+}
