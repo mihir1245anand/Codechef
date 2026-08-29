47ff35ce9036deab4a32f327ded312dd67bb5b8e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:05 +0530

    Add solution for TV Discount (TVDISC)

---
+++ TV Discount.cpp
@@ -0,0 +1,32 @@
+/**
+ * Problem Name: TV Discount
+ * Problem Code: TVDISC
+ * Problem URL: https://www.codechef.com/problems/TVDISC
+ * Submission ID: 1330679930
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C, D;
+        cin >> A >> B >> C >> D;
+
+        int first = A - C;
+        int second = B - D;
+
+        if (first < second)
+            cout << "First" << endl;
+        else if (second < first)
+            cout << "Second" << endl;
+        else
+            cout << "Any" << endl;
+    }
+
+    return 0;
+}
