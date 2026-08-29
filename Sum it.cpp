c67cefbf14c041e52949beb5ad7f81eca7440750
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:56 +0530

    Add solution for Sum it (SUMM)

---
+++ Sum it.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Sum it
+ * Problem Code: SUMM
+ * Problem URL: https://www.codechef.com/problems/SUMM
+ * Submission ID: 1330632390
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        if (A + B == C)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
