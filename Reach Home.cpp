b777ff8b8d9a3ba3111c5726579e28a9cb6cd7d4
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:43 +0530

    Add solution for Reach Home (REACH_HOME)

---
+++ Reach Home.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Reach Home
+ * Problem Code: REACH_HOME
+ * Problem URL: https://www.codechef.com/problems/REACH_HOME
+ * Submission ID: 1330667882
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (5 * X >= Y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
