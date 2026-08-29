63c03bfb50473e22f355471ac5e20688a431a8fc
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:47 +0530

    Add solution for Chess Ratings (C_RATING)

---
+++ Chess Ratings.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chess Ratings
+ * Problem Code: C_RATING
+ * Problem URL: https://www.codechef.com/problems/C_RATING
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chess Ratings (C_RATING)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
