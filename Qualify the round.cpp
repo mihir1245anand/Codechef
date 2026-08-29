43b402d467ef56a4db4a4f969ed13c4db5354b23
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:42 +0530

    Add solution for Qualify the round (QUALIFY)

---
+++ Qualify the round.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Qualify the round
+ * Problem Code: QUALIFY
+ * Problem URL: https://www.codechef.com/problems/QUALIFY
+ * Submission ID: 1343832152
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, A, B;
+        cin >> X >> A >> B;
+
+        int score = A + 2 * B;
+
+        if (score >= X)
+            cout << "Qualify\n";
+        else
+            cout << "NotQualify\n";
+    }
+
+    return 0;
+}
