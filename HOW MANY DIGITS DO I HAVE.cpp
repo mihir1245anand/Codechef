d0ba53b10094cfc794a9ede537bb995d7f36b731
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:13 +0530

    Add solution for HOW MANY DIGITS DO I HAVE (HOWMANY)

---
+++ HOW MANY DIGITS DO I HAVE.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: HOW MANY DIGITS DO I HAVE
+ * Problem Code: HOWMANY
+ * Problem URL: https://www.codechef.com/problems/HOWMANY
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: HOW MANY DIGITS DO I HAVE (HOWMANY)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
