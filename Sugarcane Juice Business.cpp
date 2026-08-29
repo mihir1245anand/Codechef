9d8804a6bf7e6015bac068e8b12842615c283f24
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:55 +0530

    Add solution for Sugarcane Juice Business (SUGARCANE)

---
+++ Sugarcane Juice Business.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Sugarcane Juice Business
+ * Problem Code: SUGARCANE
+ * Problem URL: https://www.codechef.com/problems/SUGARCANE
+ * Submission ID: 1332554381
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long N;
+        cin >> N;
+        cout << 15 * N << endl;
+    }
+
+    return 0;
+}
