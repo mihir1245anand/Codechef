1dad4bb8d4c0ecb7cf817a20df8b799ec2d38892
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:34 +0530

    Add solution for Parliament (PARLIAMENT)

---
+++ Parliament.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Parliament
+ * Problem Code: PARLIAMENT
+ * Problem URL: https://www.codechef.com/problems/PARLIAMENT
+ * Submission ID: 1330673455
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        if (2 * X >= N)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
