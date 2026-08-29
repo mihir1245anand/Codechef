4d10c39e1fa6ec248118a4e1e0aa0bc65c7f9519
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:48 +0530

    Add solution for Clear Day (CLEARDAY)

---
+++ Clear Day.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: Clear Day
+ * Problem Code: CLEARDAY
+ * Problem URL: https://www.codechef.com/problems/CLEARDAY
+ * Submission ID: 1325584392
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    cout << 7 - X - Y << endl;
+    return 0;
+}
