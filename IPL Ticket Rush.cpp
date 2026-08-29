9391d4738bb894915b848f276cb0371ea1b7937e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:16 +0530

    Add solution for IPL Ticket Rush (IPLTRSH)

---
+++ IPL Ticket Rush.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: IPL Ticket Rush
+ * Problem Code: IPLTRSH
+ * Problem URL: https://www.codechef.com/problems/IPLTRSH
+ * Submission ID: 1329308416
+ * Language: C++
+ */
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, M;
+        cin >> N >> M;
+        cout << max(0, N - M) << endl;
+    }
+
+    return 0;
+}
