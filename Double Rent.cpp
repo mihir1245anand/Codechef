c4287dfe90873656abbd8691a7952e250090ad0d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:58 +0530

    Add solution for Double Rent (DOUBLERENT)

---
+++ Double Rent.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Name: Double Rent
+ * Problem Code: DOUBLERENT
+ * Problem URL: https://www.codechef.com/problems/DOUBLERENT
+ * Submission ID: 1325585337
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+    cout << 2 * X << endl;
+    return 0;
+}
