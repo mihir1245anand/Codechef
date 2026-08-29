6cbfcfe7b1063357a98a50a1c5ed445e91e3d5b0
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:08 +0530

    Add solution for Waiting Time (WAITTIME)

---
+++ Waiting Time.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Waiting Time
+ * Problem Code: WAITTIME
+ * Problem URL: https://www.codechef.com/problems/WAITTIME
+ * Submission ID: 1330634390
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int K, X;
+        cin >> K >> X;
+
+        cout << 7 * K - X << endl;
+    }
+
+    return 0;
+}
