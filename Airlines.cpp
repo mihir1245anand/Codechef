e8cb56b3521391a4132332ed4ac9ace2e0eaf0d6
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:21 +0530

    Add solution for Airlines (SPCP2)

---
+++ Airlines.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Airlines
+ * Problem Code: SPCP2
+ * Problem URL: https://www.codechef.com/problems/SPCP2
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Airlines (SPCP2)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
