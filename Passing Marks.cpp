2dd5adf57a26982c0e2152ae27f8d26bba134eed
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:36 +0530

    Add solution for Passing Marks (PSGRADE)

---
+++ Passing Marks.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Passing Marks
+ * Problem Code: PSGRADE
+ * Problem URL: https://www.codechef.com/problems/PSGRADE
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Passing Marks (PSGRADE)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
