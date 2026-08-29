21557e51c3a84869ab7d5e1f6f177d3d6ae08220
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:47 +0530

    Add solution for Roller Coaster (MINHEIGHT)

---
+++ Roller Coaster.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Roller Coaster
+ * Problem Code: MINHEIGHT
+ * Problem URL: https://www.codechef.com/problems/MINHEIGHT
+ * Submission ID: 1330599237
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, H;
+        cin >> X >> H;
+
+        if (X >= H)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
