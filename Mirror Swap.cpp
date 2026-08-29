63dc3c71ffd32ffe57d7d2fdad63d82f423bde62
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:28 +0530

    Add solution for Mirror Swap (MRSWAP)

---
+++ Mirror Swap.cpp
@@ -0,0 +1,35 @@
+/**
+ * Problem Name: Mirror Swap
+ * Problem Code: MRSWAP
+ * Problem URL: https://www.codechef.com/problems/MRSWAP
+ * Submission ID: 1344870536
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int N;
+	    cin>>N;
+	    
+	    vector<int>A(2*N);
+	    
+	    for(int i=0;i<2*N;i++){
+	         cin>>A[i];
+	    }
+	    int sum=0;
+	    
+	     for (int i = 0; i < N; i++) {
+            sum += max(A[i], A[2 * N - 1 - i]);
+        }
+
+        cout << sum << endl;
+	}
+
+}
