30010317872709782c4f6c3df718a7bd8675b2a9
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:50 +0530

    Add solution for Secret Recipe (CHEFRUN)

---
+++ Secret Recipe.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Secret Recipe
+ * Problem Code: CHEFRUN
+ * Problem URL: https://www.codechef.com/problems/CHEFRUN
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Secret Recipe (CHEFRUN)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
