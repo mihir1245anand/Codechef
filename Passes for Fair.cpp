dd2e3f068ef9dbce0e2eef7b3f468104f547e241
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:35 +0530

    Add solution for Passes for Fair (FAIRPASS)

---
+++ Passes for Fair.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Passes for Fair
+ * Problem Code: FAIRPASS
+ * Problem URL: https://www.codechef.com/problems/FAIRPASS
+ * Submission ID: 1330641862
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        if (K >= N + 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
