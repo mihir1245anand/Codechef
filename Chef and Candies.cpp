cb6acd11345c4ae6d5eed365e84ffce15d8919cf
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:37 +0530

    Add solution for Chef and Candies (CHEFCAND)

---
+++ Chef and Candies.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Name: Chef and Candies
+ * Problem Code: CHEFCAND
+ * Problem URL: https://www.codechef.com/problems/CHEFCAND
+ * Submission ID: 1333432606
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int n,x;
+        cin>>n>>x;
+        if (x >= n) {
+            cout << 0 << endl;
+        } else {
+            int need = n - x;
+            cout << (need + 3) / 4 << endl;
+        }
+
+    }
+
+}
