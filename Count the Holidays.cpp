3516dd821af834cb8cf22d7fa012e36f54c832d4
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:50 +0530

    Add solution for Count the Holidays (SUNDAY)

---
+++ Count the Holidays.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Count the Holidays
+ * Problem Code: SUNDAY
+ * Problem URL: https://www.codechef.com/problems/SUNDAY
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Count the Holidays (SUNDAY)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
