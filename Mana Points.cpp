25382667185eeb11ed4ec33f5e2d284ad07c1555
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:21 +0530

    Add solution for Mana Points (MANAPTS)

---
+++ Mana Points.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Mana Points
+ * Problem Code: MANAPTS
+ * Problem URL: https://www.codechef.com/problems/MANAPTS
+ * Submission ID: 1330636739
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << Y / X << endl;
+    }
+
+    return 0;
+}
