6878169087554b65b1cbbc2000e84a2eeba38e2a
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:11 +0530

    Add solution for Group Assignment (GROUPASSGN)

---
+++ Group Assignment.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Group Assignment
+ * Problem Code: GROUPASSGN
+ * Problem URL: https://www.codechef.com/problems/GROUPASSGN
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Group Assignment (GROUPASSGN)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
