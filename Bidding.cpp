6f87a7d61ffedd3ed82d5f3a6a572724ff6aa89b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:27 +0530

    Add solution for Bidding (AUCTION)

---
+++ Bidding.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Bidding
+ * Problem Code: AUCTION
+ * Problem URL: https://www.codechef.com/problems/AUCTION
+ * Submission ID: 1330640206
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int A, B, C;
+        cin >> A >> B >> C;
+
+        if (A > B && A > C)
+            cout << "Alice" << endl;
+        else if (B > A && B > C)
+            cout << "Bob" << endl;
+        else
+            cout << "Charlie" << endl;
+    }
+
+    return 0;
+}
