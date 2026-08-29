ba535097f928b16f3ace9fc3c467c2b4c6639d7a
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:45 +0530

    Add solution for Read Pages (READPAGES)

---
+++ Read Pages.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Read Pages
+ * Problem Code: READPAGES
+ * Problem URL: https://www.codechef.com/problems/READPAGES
+ * Submission ID: 1330642501
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X, Y;
+        cin >> N >> X >> Y;
+
+        if (X * Y >= N)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
