288da922d0cdd6c1d5fc9c66f407aaef4de6710b
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:10 +0530

    Add solution for Good Investment or Not (INVESTMENT)

---
+++ Good Investment or Not.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Good Investment or Not
+ * Problem Code: INVESTMENT
+ * Problem URL: https://www.codechef.com/problems/INVESTMENT
+ * Submission ID: 1330658954
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X >= 2 * Y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
