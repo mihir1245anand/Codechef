9e513d9cf65674e83a7890a0386d921631d7be7f
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:56 +0530

    Add solution for Determine the Score (DETSCORE)

---
+++ Determine the Score.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Determine the Score
+ * Problem Code: DETSCORE
+ * Problem URL: https://www.codechef.com/problems/DETSCORE
+ * Submission ID: 1325587492
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, N;
+        cin >> X >> N;
+
+        cout << (X / 10) * N << endl;
+    }
+
+    return 0;
+}
