9fe741c4cfcde6cc67c47069071bc4a8f3690a64
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:06 +0530

    Add solution for Find the Direction (FACEDIR)

---
+++ Find the Direction.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Find the Direction
+ * Problem Code: FACEDIR
+ * Problem URL: https://www.codechef.com/problems/FACEDIR
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Find the Direction (FACEDIR)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
