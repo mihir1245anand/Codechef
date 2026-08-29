9686d63646ac509627b5e9feee8cd89755044cd0
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:19 +0530

    Add solution for Kitchen Timings (KITCHENTIME)

---
+++ Kitchen Timings.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Kitchen Timings
+ * Problem Code: KITCHENTIME
+ * Problem URL: https://www.codechef.com/problems/KITCHENTIME
+ * Submission ID: 1325589152
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << Y - X << endl;
+    }
+
+    return 0;
+}
