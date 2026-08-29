c9979eeb952d76f7b55b36be1f65bf3ca006d6b2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:58 +0530

    Add solution for Donation Rewards (DOREWARD)

---
+++ Donation Rewards.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Donation Rewards
+ * Problem Code: DOREWARD
+ * Problem URL: https://www.codechef.com/problems/DOREWARD
+ * Submission ID: 1330668684
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X <= 3)
+            cout << "BRONZE" << endl;
+        else if (X <= 6)
+            cout << "SILVER" << endl;
+        else
+            cout << "GOLD" << endl;
+    }
+
+    return 0;
+}
