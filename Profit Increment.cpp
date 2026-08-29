138770612318718e3a82e54cca1b1efe8350dfc8
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:41 +0530

    Add solution for Profit Increment (PROINC)

---
+++ Profit Increment.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Profit Increment
+ * Problem Code: PROINC
+ * Problem URL: https://www.codechef.com/problems/PROINC
+ * Submission ID: 1330673081
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << Y + X / 10 << endl;
+    }
+
+    return 0;
+}
