dee2eb0159c2577f54093f5d7496ceffc836a301
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:04 +0530

    Add solution for Fever (FEVER)

---
+++ Fever.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Fever
+ * Problem Code: FEVER
+ * Problem URL: https://www.codechef.com/problems/FEVER
+ * Submission ID: 1330646932
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int x;
+	    cin>>x;
+	    if(x>98){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+
+}
