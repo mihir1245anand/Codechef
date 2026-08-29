cf28c1b3a9013f14016f2995dd0df917a44bfd93
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:54 +0530

    Add solution for Speed Limit Test (SPEEDTEST)

---
+++ Speed Limit Test.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Speed Limit Test
+ * Problem Code: SPEEDTEST
+ * Problem URL: https://www.codechef.com/problems/SPEEDTEST
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Speed Limit Test (SPEEDTEST)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
