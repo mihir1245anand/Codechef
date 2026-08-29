5012df6ad8cb81ae589930e3af6f16db145ebf4e
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:10 +0530

    Add solution for Water Requirement (WATERREQ)

---
+++ Water Requirement.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Water Requirement
+ * Problem Code: WATERREQ
+ * Problem URL: https://www.codechef.com/problems/WATERREQ
+ * Submission ID: 1330656439
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int N;
+        cin >> N;
+
+        cout << 2 * N << endl;
+    }
+
+}
