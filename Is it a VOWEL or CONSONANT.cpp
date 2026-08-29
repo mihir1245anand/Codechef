bf46cf526354b1a17dc9877677fac8bb06e9a84d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:16 +0530

    Add solution for Is it a VOWEL or CONSONANT (VOWELTB)

---
+++ Is it a VOWEL or CONSONANT.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Is it a VOWEL or CONSONANT
+ * Problem Code: VOWELTB
+ * Problem URL: https://www.codechef.com/problems/VOWELTB
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Is it a VOWEL or CONSONANT (VOWELTB)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
