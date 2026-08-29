fd5a79a5cb4d6d5de0e0adc5047d18391a2cd8aa
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:52 +0530

    Add solution for Small Factorial (FLOW018)

---
+++ Small Factorial.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Small Factorial
+ * Problem Code: FLOW018
+ * Problem URL: https://www.codechef.com/problems/FLOW018
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Small Factorial (FLOW018)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
