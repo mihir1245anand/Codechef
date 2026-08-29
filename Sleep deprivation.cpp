3e6e9356743dfd38325ef4742038c1b2755c1619
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:52 +0530

    Add solution for Sleep deprivation (SLEEP)

---
+++ Sleep deprivation.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Sleep deprivation
+ * Problem Code: SLEEP
+ * Problem URL: https://www.codechef.com/problems/SLEEP
+ * Submission ID: 1330647338
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X < 7)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
