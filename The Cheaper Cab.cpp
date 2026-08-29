1487fd9f5ec976830dc9c3b402b1fceaec27b106
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:59 +0530

    Add solution for The Cheaper Cab (CABS)

---
+++ The Cheaper Cab.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: The Cheaper Cab
+ * Problem Code: CABS
+ * Problem URL: https://www.codechef.com/problems/CABS
+ * Submission ID: 1330669091
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X < Y)
+            cout << "FIRST" << endl;
+        else if (Y < X)
+            cout << "SECOND" << endl;
+        else
+            cout << "ANY" << endl;
+    }
+
+    return 0;
+}
