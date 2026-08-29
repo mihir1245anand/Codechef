74b706de0655a32a5aa33599147065f7282e249d
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:40 +0530

    Add solution for Chef and his Apps (CHEFAPPS)

---
+++ Chef and his Apps.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Chef and his Apps
+ * Problem Code: CHEFAPPS
+ * Problem URL: https://www.codechef.com/problems/CHEFAPPS
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Chef and his Apps (CHEFAPPS)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
