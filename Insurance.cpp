f07e9540f37e7f9b950710431e0e22f0242c90fd
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:14 +0530

    Add solution for Insurance (INSURANCE)

---
+++ Insurance.cpp
@@ -0,0 +1,24 @@
+/**
+ * Problem Name: Insurance
+ * Problem Code: INSURANCE
+ * Problem URL: https://www.codechef.com/problems/INSURANCE
+ * Submission ID: 1331107814
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << min(X, Y) << endl;
+    }
+
+    return 0;
+}
