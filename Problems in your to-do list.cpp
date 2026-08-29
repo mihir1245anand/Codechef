9dd503b4aadc6966835c4745b528ea95929d0eb1
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:40 +0530

    Add solution for Problems in your to-do list (TODOLIST)

---
+++ Problems in your to-do list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Problem Name: Problems in your to-do list
+ * Problem Code: TODOLIST
+ * Problem URL: https://www.codechef.com/problems/TODOLIST
+ * Submission ID: 1342658716
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            int d;
+            cin >> d;
+            if (d >= 1000)
+                count++;
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
