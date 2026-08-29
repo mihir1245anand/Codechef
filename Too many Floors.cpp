f4f544b05ac4acd0fed594831501ddd1b6ff1e40
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:18:03 +0530

    Add solution for Too many Floors (FLOORS)

---
+++ Too many Floors.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Too many Floors
+ * Problem Code: FLOORS
+ * Problem URL: https://www.codechef.com/problems/FLOORS
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Too many Floors (FLOORS)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
