8845829b931064afa1112cebe4c615ac88be4419
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:57 +0530

    Add solution for Donation Drive (DONDRIVE)

---
+++ Donation Drive.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Donation Drive
+ * Problem Code: DONDRIVE
+ * Problem URL: https://www.codechef.com/problems/DONDRIVE
+ * Submission ID: 1325588491
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+        cout << N - X << endl;
+    }
+
+    return 0;
+}
