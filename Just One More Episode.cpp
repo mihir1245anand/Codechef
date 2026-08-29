a2bcc4c3f97dce27ed527ad18a84129249f331fb
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:18 +0530

    Add solution for Just One More Episode (ONEMORE)

---
+++ Just One More Episode.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Just One More Episode
+ * Problem Code: ONEMORE
+ * Problem URL: https://www.codechef.com/problems/ONEMORE
+ * Submission ID: 1330635404
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X > 24)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
