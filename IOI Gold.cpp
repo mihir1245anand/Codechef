776de72c3fc6bf3856ceebe5345ceebc879e1dcc
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:15 +0530

    Add solution for IOI Gold (IOI7)

---
+++ IOI Gold.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: IOI Gold
+ * Problem Code: IOI7
+ * Problem URL: https://www.codechef.com/problems/IOI7
+ * Submission ID: 1344860915
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N,G;
+	cin>>N>>G;
+	
+	if(N>=G){
+	    cout<<"Yes";
+	}
+	else{
+	    cout<<"No";
+	}
+	return 0;
+
+}
