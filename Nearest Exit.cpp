1e524d0caf936dbf36ec721fc4d0acada3cf0be3
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:31 +0530

    Add solution for Nearest Exit (NEARESTEXIT)

---
+++ Nearest Exit.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Nearest Exit
+ * Problem Code: NEARESTEXIT
+ * Problem URL: https://www.codechef.com/problems/NEARESTEXIT
+ * Submission ID: 1343019365
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X <= 50)
+            cout << "LEFT" << endl;
+        else
+            cout << "RIGHT" << endl;
+    }
+
+    return 0;
+}
