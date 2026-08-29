4d44f7f51ce88b6b7c47251c3415e10edf1771d9
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:54 +0530

    Add solution for Cyclic Quadrilateral (CYCLICQD)

---
+++ Cyclic Quadrilateral.cpp
@@ -0,0 +1,27 @@
+/**
+ * Problem Name: Cyclic Quadrilateral
+ * Problem Code: CYCLICQD
+ * Problem URL: https://www.codechef.com/problems/CYCLICQD
+ * Track: 500 to 1000 difficulty rating
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Problem: Cyclic Quadrilateral (CYCLICQD)
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
