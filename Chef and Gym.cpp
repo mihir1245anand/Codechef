28d24a92f7a9f80741fa3375b596f915360078f2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:39 +0530

    Add solution for Chef and Gym (CGYM)

---
+++ Chef and Gym.cpp
@@ -0,0 +1,29 @@
+/**
+ * Problem Name: Chef and Gym
+ * Problem Code: CGYM
+ * Problem URL: https://www.codechef.com/problems/CGYM
+ * Submission ID: 1331116970
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        if (Z >= X + Y)
+            cout << 2 << endl;
+        else if (Z >= X)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+}
