86cf7fe4fd3d4583bac56f5d1e760e8445ec4508
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:17:02 +0530

    Add solution for EXAMCHEF (EXAMCHEF)

---
+++ EXAMCHEF.cpp
@@ -0,0 +1,25 @@
+/**
+ * Problem Name: EXAMCHEF
+ * Problem Code: EXAMCHEF
+ * Problem URL: https://www.codechef.com/problems/EXAMCHEF
+ * Submission ID: 1332564944
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    
+	     if (2 * z > x * y)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+}
