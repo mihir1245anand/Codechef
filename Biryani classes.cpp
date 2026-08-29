f982e6c5978e2e6c1432c0d67430e852e1b143f2
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:28 +0530

    Add solution for Biryani classes (BIRYANI)

---
+++ Biryani classes.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Biryani classes
+ * Problem Code: BIRYANI
+ * Problem URL: https://www.codechef.com/problems/BIRYANI
+ * Submission ID: 1325586390
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << X * Y << endl;
+    }
+
+    return 0;
+}
