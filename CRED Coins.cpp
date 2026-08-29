3ccea037d8f62ea88fe84125e5a17bccbc9db0c4
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:52 +0530

    Add solution for CRED Coins (CREDCOINS)

---
+++ CRED Coins.cpp
@@ -0,0 +1,21 @@
+/**
+ * Problem Name: CRED Coins
+ * Problem Code: CREDCOINS
+ * Problem URL: https://www.codechef.com/problems/CREDCOINS
+ * Submission ID: 1325597125
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << (X * Y) / 100 << endl;
+    }
+    return 0;
+}
