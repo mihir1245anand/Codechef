a28cbcf304a699e598acd5980cd511518af8d0bc
raghav1245anand <raghavanand034@gmail.com>
2026-08-29 18:16:50 +0530

    Add solution for Count the Notebooks (NOTEBOOK)

---
+++ Count the Notebooks.cpp
@@ -0,0 +1,23 @@
+/**
+ * Problem Name: Count the Notebooks
+ * Problem Code: NOTEBOOK
+ * Problem URL: https://www.codechef.com/problems/NOTEBOOK
+ * Submission ID: 1333428635
+ * Language: C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+        cout << N * 10 << endl;
+    }
+
+    return 0;
+}
