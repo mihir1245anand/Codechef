6dd53a8d2c8bd2dc9dc814a7adb8afa7712a24f6
raghav1245anand <raghavanand034@gmail.com>
2026-09-06 21:39:37 +0530

    Add solution for Reach Weight (REACHWT)

---
+++ Reach Weight.cpp
@@ -0,0 +1,28 @@
+/**
+ * Problem Name: Reach Weight
+ * Problem Code: REACHWT
+ * Problem URL: https://www.codechef.com/problems/REACHWT
+ * Submission ID: 1352018414
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int cost=(n/2)*30+(n%2)*20;
+        cout<<cost<<"\n";
+    }
+    return 0;
+
+}
