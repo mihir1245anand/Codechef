afcbab6df2400d906960d2af1fbaeae3f1029ca1
raghav1245anand <raghavanand034@gmail.com>
2026-09-06 21:39:37 +0530

    Add solution for Missing Number (MISSINGNUM7)

---
+++ Missing Number.cpp
@@ -0,0 +1,19 @@
+/**
+ * Problem Name: Missing Number
+ * Problem Code: MISSINGNUM7
+ * Problem URL: https://www.codechef.com/problems/MISSINGNUM7
+ * Submission ID: 1352015083
+ * Language: C++
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int s;
+	cin>>s;
+	cout<<10-s;
+	
+
+}
